a43f9dbf976ee22abd2d647202427ea8ad9a7a0f
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2022-04-01 06:24:12 +0200

    INTYGFV-14393: Initial commit of core repo including customer-terminate-service.

---
+++ customer-terminate-service/app/src/main/java/se/inera/intyg/cts/CustomerTerminateServiceApplication.java
@@ -0,0 +1,14 @@
+package se.inera.intyg.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CustomerTerminateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerTerminateServiceApplication.class, args);
+    }
+
+}
---
+++ customer-terminate-service/app/src/test/java/se/inera/intyg/cts/CustomerTerminateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.inera.intyg.cts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTerminateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
