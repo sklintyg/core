17d0ad20af7342c77b88169cf633f161a09baccf
Karl Friberg <28860194+IamFriberg@users.noreply.github.com>
2022-04-14 14:09:41 +0200

    INTYGFV-14393: Added formating for LocalDateTime because Spring can not handle some Java 8 classes.

--- customer-terminate-service/app/src/main/java/se/inera/intyg/cts/application/dto/TerminationDTO.java
+++ customer-terminate-service/app/src/main/java/se/inera/intyg/cts/application/dto/TerminationDTO.java
@@ -1,10 +1,11 @@
 package se.inera.intyg.cts.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record TerminationDTO(UUID terminationId,
-                             LocalDateTime created,
+                             @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss") LocalDateTime created, //JSON formating for JAVA 8 types
                              String creatorHSAId,
                              String creatorName,
                              String status,
