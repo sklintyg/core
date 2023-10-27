9c44b4ce362dd0b0f66330e44a7863191e04c0c4
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-10-27 11:03:41 +0200

    INTYGFV-16011: Format code

--- intyg-proxy-service/app/src/test/java/se/inera/intyg/intygproxyservice/employee/EmployeeServiceTest.java
+++ intyg-proxy-service/app/src/test/java/se/inera/intyg/intygproxyservice/employee/EmployeeServiceTest.java
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 package se.inera.intyg.intygproxyservice.employee;
 
 import static org.junit.jupiter.api.Assertions.*;
--- intyg-proxy-service/integration-hsa/src/test/java/se/inera/intyg/intygproxyservice/integration/employee/HsaGetEmployeeIntegrationServiceTest.java
+++ intyg-proxy-service/integration-hsa/src/test/java/se/inera/intyg/intygproxyservice/integration/employee/HsaGetEmployeeIntegrationServiceTest.java
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 package se.inera.intyg.intygproxyservice.integration.employee;
 
 import static org.junit.jupiter.api.Assertions.*;
