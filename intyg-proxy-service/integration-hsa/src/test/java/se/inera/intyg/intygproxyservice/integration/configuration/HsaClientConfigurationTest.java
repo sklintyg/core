2aba1ac5253ddc9ccf88ba11597d3001f085a013
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2023-11-01 07:34:57 +0100

    INTYGFV-16029: Added configuration for hsa webservices. Refactored common integration code to integration-common module. Restructured integration properties.

---
+++ intyg-proxy-service/integration-hsa/src/test/java/se/inera/intyg/intygproxyservice/integration/configuration/HsaClientConfigurationTest.java
@@ -0,0 +1,94 @@
+package se.inera.intyg.intygproxyservice.integration.configuration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import se.inera.intyg.intygproxyservice.integration.common.WebServiceClientFactory;
+import se.riv.infrastructure.directory.employee.getemployeeincludingprotectedperson.v3.rivtabp21.GetEmployeeIncludingProtectedPersonResponderInterface;
+import se.riv.infrastructure.directory.organization.gethealthcareunit.v2.rivtabp21.GetHealthCareUnitResponderInterface;
+import se.riv.infrastructure.directory.organization.gethealthcareunitmembers.v2.rivtabp21.GetHealthCareUnitMembersResponderInterface;
+
+@ExtendWith(MockitoExtension.class)
+class HsaClientConfigurationTest {
+
+  public static final String GET_EMPLOYEE_INCLUDING_PROTECTED_PERSON_ENDPOINT = "GET_EMPLOYEE_INCLUDING_PROTECTED_PERSON_ENDPOINT";
+  public static final String GET_HEALTH_CARE_UNIT_ENDPOINT = "GET_HEALTH_CARE_UNIT_ENDPOINT";
+  public static final String GET_HEALTH_CARE_UNIT_MEMBERS_ENDPOINT = "GET_HEALTH_CARE_UNIT_MEMBERS_ENDPOINT";
+  @Mock
+  private WebServiceClientFactory webServiceClientFactory;
+
+  @InjectMocks
+  private HsaClientConfiguration hsaClientConfiguration;
+
+  @BeforeEach
+  void setUp() {
+    ReflectionTestUtils.setField(
+        hsaClientConfiguration,
+        "getEmployeeIncludingProtectedPersonEndpoint",
+        GET_EMPLOYEE_INCLUDING_PROTECTED_PERSON_ENDPOINT
+    );
+    ReflectionTestUtils.setField(
+        hsaClientConfiguration,
+        "getHealthCareUnitEndpoint",
+        GET_HEALTH_CARE_UNIT_ENDPOINT
+    );
+    ReflectionTestUtils.setField(
+        hsaClientConfiguration,
+        "getHealthCareUnitMembersEndpoint",
+        GET_HEALTH_CARE_UNIT_MEMBERS_ENDPOINT
+    );
+  }
+
+  @Test
+  void shallReturnGetEmployeeIncludingProtectedPerson() {
+    final var expected = mock(GetEmployeeIncludingProtectedPersonResponderInterface.class);
+
+    doReturn(expected)
+        .when(webServiceClientFactory)
+        .create(
+            GetEmployeeIncludingProtectedPersonResponderInterface.class,
+            GET_EMPLOYEE_INCLUDING_PROTECTED_PERSON_ENDPOINT
+        );
+
+    final var actual = hsaClientConfiguration.getEmployeeIncludingProtectedPerson();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void shallReturnGetHealthCareUnit() {
+    final var expected = mock(GetHealthCareUnitResponderInterface.class);
+
+    doReturn(expected)
+        .when(webServiceClientFactory)
+        .create(
+            GetHealthCareUnitResponderInterface.class,
+            GET_HEALTH_CARE_UNIT_ENDPOINT
+        );
+
+    final var actual = hsaClientConfiguration.getHealthCareUnitResponderInterface();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void shallReturnGetHealthCareUnitMembers() {
+    final var expected = mock(GetHealthCareUnitMembersResponderInterface.class);
+
+    doReturn(expected)
+        .when(webServiceClientFactory)
+        .create(
+            GetHealthCareUnitMembersResponderInterface.class,
+            GET_HEALTH_CARE_UNIT_MEMBERS_ENDPOINT
+        );
+
+    final var actual = hsaClientConfiguration.getHealthCareUnitMembersResponderInterface();
+    assertEquals(expected, actual);
+  }
+}
