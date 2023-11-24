e62ed20de8201193444664c32e1b13beac218fe8
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-11-24 15:41:20 +0100

    INTYGFV-16137: Implemented get credential information

---
+++ intyg-proxy-service/app/src/main/java/se/inera/intyg/intygproxyservice/authorization/CredentialInformationController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.intygproxyservice.authorization;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.inera.intyg.intygproxyservice.authorization.dto.CredentialInformationRequest;
+import se.inera.intyg.intygproxyservice.authorization.dto.CredentialInformationResponse;
+import se.inera.intyg.intygproxyservice.authorization.service.CredentialInformationService;
+
+@RestController()
+@RequestMapping("/api/v1/credentialinformation")
+@AllArgsConstructor
+public class CredentialInformationController {
+
+  private final CredentialInformationService credentialInformationService;
+
+  @PostMapping
+  CredentialInformationResponse getCredentialInformation(
+      @RequestBody CredentialInformationRequest request) {
+    return credentialInformationService.get(request);
+  }
+}
