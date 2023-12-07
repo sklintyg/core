25df3b689e632410f595bbb9191b3523b5657d58
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2023-12-07 10:56:08 +0100

    INTYGFV-16180: Updated test

--- intyg-proxy-service/app/src/test/java/se/inera/intyg/intygproxyservice/authorization/HandleCertificationPersonControllerTest.java
+++ intyg-proxy-service/app/src/test/java/se/inera/intyg/intygproxyservice/authorization/HandleCertificationPersonControllerTest.java
@@ -24,12 +24,15 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import se.inera.intyg.intygproxyservice.authorization.dto.HandleCertificationPersonRequest;
 import se.inera.intyg.intygproxyservice.authorization.dto.HandleCertificationPersonResponse;
 import se.inera.intyg.intygproxyservice.authorization.service.HandleCertificationPersonService;
 
+@ExtendWith(MockitoExtension.class)
 class HandleCertificationPersonControllerTest {
 
   @Mock
