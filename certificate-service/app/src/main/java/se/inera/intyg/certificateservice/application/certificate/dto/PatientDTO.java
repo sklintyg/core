f9c2214e4ecfe2c2ada80583d251c28926c09ad6
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2024-02-02 11:03:07 +0100

    INTYGFV-16326: Use correct PatientDTO for responses

---
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/dto/PatientDTO.java
@@ -0,0 +1,36 @@
+package se.inera.intyg.certificateservice.application.certificate.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
+import lombok.Value;
+import se.inera.intyg.certificateservice.application.certificate.dto.PatientDTO.PatientBuilderDTOBuilder;
+
+@JsonDeserialize(builder = PatientBuilderDTOBuilder.class)
+@Value
+@Builder
+public class PatientDTO {
+
+  PersonIdDTO personId;
+  PersonIdDTO previousPersonId;
+  String firstName;
+  String lastName;
+  String middleName;
+  String fullName;
+  String street;
+  String city;
+  String zipCode;
+  boolean coordinationNumber;
+  boolean testIndicated;
+  boolean protectedPerson;
+  boolean deceased;
+  boolean differentNameFromEHR;
+  boolean personIdChanged;
+  boolean reserveId;
+  boolean addressFromPU;
+
+  @JsonPOJOBuilder(withPrefix = "")
+  public static class PatientBuilderDTOBuilder {
+
+  }
+}
