59ae8ebea6ae34cd3d969af9057015e0711a6774
Mikaela Romild <mikaela.romild@soprasteria.com>
2024-02-07 13:56:51 +0100

    INTYGFV-16352: Added CertificateDataEntity

---
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificate/persistence/entity/CertificateDataEntity.java
@@ -0,0 +1,60 @@
+package se.inera.intyg.certificateservice.infrastructure.certificate.persistence.entity;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import java.nio.charset.StandardCharsets;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "certificate_data")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CertificateDataEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "key")
+  private Long key;
+
+  @Column(name = "certificate_key")
+  private Long certificateKey;
+
+  @Lob
+  @Basic(fetch = FetchType.LAZY)
+  @Column(name = "data")
+  private byte[] data;
+
+  public CertificateDataEntity(String data) {
+    this.data = toBytes(data);
+  }
+
+  public void setData(String document) {
+    this.data = toBytes(document);
+  }
+
+  public String getData() {
+    return fromBytes(this.data);
+  }
+
+  private byte[] toBytes(String data) {
+    if (data == null) {
+      return new byte[0];
+    }
+    return data.getBytes(StandardCharsets.UTF_8);
+  }
+
+  private String fromBytes(byte[] bytes) {
+    return new String(bytes, StandardCharsets.UTF_8);
+  }
+
+}
