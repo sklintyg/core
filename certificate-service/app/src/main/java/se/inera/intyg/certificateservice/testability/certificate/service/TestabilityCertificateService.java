f18ecf5fa917686fd4374aae92a150924f77d1cb
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2024-02-08 08:46:05 +0100

    INTYGFV-16359: Added support to get supported certificate types in testability. (#94)

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/testability/certificate/service/TestabilityCertificateService.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/testability/certificate/service/TestabilityCertificateService.java
@@ -2,6 +2,10 @@ package se.inera.intyg.certificateservice.testability.certificate.service;
 
 import static se.inera.intyg.certificateservice.testability.common.TestabilityConstants.TESTABILITY_PROFILE;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -9,11 +13,15 @@ import se.inera.intyg.certificateservice.application.certificate.dto.CreateCerti
 import se.inera.intyg.certificateservice.application.certificate.service.CertificateConverter;
 import se.inera.intyg.certificateservice.application.common.ActionEvaluationFactory;
 import se.inera.intyg.certificateservice.domain.certificate.model.CertificateId;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModelId;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateType;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateVersion;
+import se.inera.intyg.certificateservice.testability.certificate.dto.SupportedCertificateTypesResponse;
 import se.inera.intyg.certificateservice.testability.certificate.dto.TestabilityCertificateRequest;
+import se.inera.intyg.certificateservice.testability.certificate.dto.TestabilityFillTypeDTO;
 import se.inera.intyg.certificateservice.testability.certificate.dto.TestabilityResetCertificateRequest;
+import se.inera.intyg.certificateservice.testability.certificate.dto.TestabilityStatusTypeDTO;
 import se.inera.intyg.certificateservice.testability.certificate.service.repository.TestabilityCertificateModelRepository;
 import se.inera.intyg.certificateservice.testability.certificate.service.repository.TestabilityCertificateRepository;
 
@@ -68,5 +76,28 @@ public class TestabilityCertificateService {
             .map(CertificateId::new)
             .toList()
     );
+  }
+
+  public List<SupportedCertificateTypesResponse> supportedTypes() {
+    final var supportedCertificateTypesResponseMap = testabilityCertificateModelRepository.all()
+        .stream()
+        .collect(Collectors.toMap(
+            CertificateModel::name,
+            certificateModel ->
+                SupportedCertificateTypesResponse.builder()
+                    .type(certificateModel.id().type().type())
+                    .internalType(certificateModel.id().type().type())
+                    .versions(List.of(certificateModel.id().version().version()))
+                    .name(certificateModel.name())
+                    .fillType(Arrays.asList(TestabilityFillTypeDTO.values()))
+                    .statuses(Arrays.asList(TestabilityStatusTypeDTO.values()))
+                    .build(),
+            (existingValue, newValue) -> {
+              final var combinedVersions = new ArrayList<>(existingValue.getVersions());
+              combinedVersions.addAll(newValue.getVersions());
+              return existingValue.withVersions(combinedVersions);
+            }
+        ));
+    return List.copyOf(supportedCertificateTypesResponseMap.values());
   }
 }
