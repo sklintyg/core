68010ba1da544f8e5c44727fc02dfc57a77adeb8
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-02-12 20:55:39 +0100

    INTYGFV-16352: Refactor

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/CertificateModelId.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/CertificateModelId.java
@@ -2,11 +2,9 @@ package se.inera.intyg.certificateservice.domain.certificatemodel.model;
 
 import lombok.Builder;
 import lombok.Value;
-import lombok.experimental.Accessors;
 
 @Value
 @Builder
-@Accessors(fluent = true)
 public class CertificateModelId {
 
   CertificateType type;
