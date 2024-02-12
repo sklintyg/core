3d08fe216efbe2044ad7d65e05ebc6d4b8741395
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-02-12 20:49:26 +0100

    INTYGFV-16352: Refactor

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificate/persistence/entity/mapper/PatientEntityMapper.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificate/persistence/entity/mapper/PatientEntityMapper.java
@@ -1,14 +1,15 @@
 package se.inera.intyg.certificateservice.infrastructure.certificate.persistence.entity.mapper;
 
-import se.inera.intyg.certificateservice.domain.certificate.model.HsaId;
-import se.inera.intyg.certificateservice.domain.certificate.model.Staff;
 import se.inera.intyg.certificateservice.domain.patient.model.Patient;
 import se.inera.intyg.certificateservice.infrastructure.certificate.persistence.entity.PatientEntity;
 import se.inera.intyg.certificateservice.infrastructure.certificate.persistence.entity.PatientIdTypeEntity;
 import se.inera.intyg.certificateservice.infrastructure.certificate.persistence.entity.PersonIdType;
-import se.inera.intyg.certificateservice.infrastructure.certificate.persistence.entity.StaffEntity;
 
 public class PatientEntityMapper {
+
+  private PatientEntityMapper() {
+    throw new IllegalStateException("Utility class");
+  }
 
   public static PatientEntity toEntity(Patient patient) {
     final var type = PersonIdType.valueOf(patient.id().type().name());
