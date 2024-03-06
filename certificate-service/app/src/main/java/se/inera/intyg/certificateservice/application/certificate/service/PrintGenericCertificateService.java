89454189c350753a5676866a2c46cc59632d6f5f
Mikaela Romild <mikaela.romild@soprasteria.com>
2024-03-06 15:55:39 +0100

    INTYGFV-16412: Added text, rectangle and line and set pdf size

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/PrintGenericCertificateService.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/PrintGenericCertificateService.java
@@ -1,10 +1,45 @@
 package se.inera.intyg.certificateservice.application.certificate.service;
 
+import lombok.SneakyThrows;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
 public class PrintGenericCertificateService {
 
-
+  @SneakyThrows
   public void get() {
 
-  }
+    final var pdfDocument = new PDDocument();
+    PDPage page = new PDPage(PDRectangle.A4);
+    pdfDocument.addPage(page);
+    PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page);
 
+    contentStream.setNonStrokingColor(235, 235, 235);
+    contentStream.addRect(50, 200, 500, 100);
+    contentStream.fill();
+    contentStream.setNonStrokingColor(0, 0, 0);
+    contentStream.beginText();
+    contentStream.setFont(PDType1Font.HELVETICA, 10);
+    contentStream.newLineAtOffset(260, 240);
+    contentStream.showText("Text on light gray rectangle");
+
+    contentStream.newLineAtOffset(100, 100);
+    contentStream.showText("____________________________");
+
+    contentStream.endText();
+    PDImageXObject pdImage = PDImageXObject.createFromFile(
+        "C:\\Intyg\\core\\certificate-service\\app\\src\\main\\resources\\transportstyrelsen_logga.png",
+        pdfDocument);
+
+    contentStream.drawImage(pdImage, 10, 700, 160, 100);
+
+    contentStream.close();
+    pdfDocument.save("generic.pdf");
+
+    pdfDocument.close();
+  }
 }
