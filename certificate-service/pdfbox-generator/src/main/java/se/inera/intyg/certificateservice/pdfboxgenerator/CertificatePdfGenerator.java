7804e20ce59f5b5c9eb65ad4191902487dfe1077
Mikaela Romild <mikaela.romild@soprasteria.com>
2024-03-25 12:59:15 +0100

    INTYGFV-16453: Replaced try catch with exception i method signatures and added try catch with resource

--- certificate-service/pdfbox-generator/src/main/java/se/inera/intyg/certificateservice/pdfboxgenerator/CertificatePdfGenerator.java
+++ certificate-service/pdfbox-generator/src/main/java/se/inera/intyg/certificateservice/pdfboxgenerator/CertificatePdfGenerator.java
@@ -22,6 +22,7 @@ import static se.inera.intyg.certificateservice.pdfboxgenerator.FK7211PdfConstan
 import java.awt.Color;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
@@ -48,8 +49,7 @@ public class CertificatePdfGenerator implements PdfGenerator {
   public final ClassLoader classLoader = getClass().getClassLoader();
 
   public Pdf generate(Certificate certificate) {
-
-    try {
+    try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
       File pdfTemplate = new File(classLoader.getResource(
           certificate.certificateModel().pdfTemplatePath()).getFile());
       PDDocument fk7211Pdf = Loader.loadPDF(pdfTemplate);
@@ -78,267 +78,209 @@ public class CertificatePdfGenerator implements PdfGenerator {
         setDraftWatermark(fk7211Pdf, page);
       }
 
-      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
       fk7211Pdf.save(byteArrayOutputStream);
       fk7211Pdf.save("fk7211_test.pdf");
       fk7211Pdf.close();
-      final var fileName = setFileName(certificate);
-      return new Pdf(byteArrayOutputStream.toByteArray(), fileName);
+
+      return new Pdf(byteArrayOutputStream.toByteArray(), setFileName(certificate));
 
     } catch (Exception e) {
-      throw new IllegalStateException("Could not create Pdf");
+      throw new IllegalStateException("Could not create Pdf", e);
     }
   }
 
-  private void setPatientInformation(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var patientNameField = acroForm.getField(PATIENT_NAME_FIELD_ID);
-      patientNameField.setValue(certificate.certificateMetaData().patient().name().fullName());
+  private void setPatientInformation(PDAcroForm acroForm, Certificate certificate)
+      throws IOException {
+    final var patientNameField = acroForm.getField(PATIENT_NAME_FIELD_ID);
+    patientNameField.setValue(certificate.certificateMetaData().patient().name().fullName());
 
-      final var patientIdField = acroForm.getField(PATIENT_ID_FIELD_ID);
-      patientIdField.setValue(certificate.certificateMetaData().patient().id().id());
+    final var patientIdField = acroForm.getField(PATIENT_ID_FIELD_ID);
+    patientIdField.setValue(certificate.certificateMetaData().patient().id().id());
 
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set patient information");
-    }
   }
 
-  private void setExpectedDeliveryDate(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var expectedDeliveryDate = acroForm.getField(
-          QUESTION_BERAKNAT_NEDKOMSTDATUM_DATE_FIELD_ID);
+  private void setExpectedDeliveryDate(PDAcroForm acroForm, Certificate certificate)
+      throws IOException {
+    final var expectedDeliveryDate = acroForm.getField(
+        QUESTION_BERAKNAT_NEDKOMSTDATUM_DATE_FIELD_ID);
 
-      if (!certificate.elementData().isEmpty()) {
-        final var dateValue = certificate.elementData().get(0).value();
+    if (!certificate.elementData().isEmpty()) {
+      final var dateValue = certificate.elementData().get(0).value();
 
-        if (dateValue instanceof ElementValueDate elementValueDate) {
-          expectedDeliveryDate.setValue((elementValueDate).date().toString());
-        }
+      if (dateValue instanceof ElementValueDate elementValueDate) {
+        expectedDeliveryDate.setValue((elementValueDate).date().toString());
       }
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set expected delivery date");
     }
   }
 
-  private void setIssuerRole(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var role = certificate.certificateMetaData().issuer().role();
+  private void setIssuerRole(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var role = certificate.certificateMetaData().issuer().role();
 
-      switch (role) {
-        case DOCTOR:
-          final var certifierDoctor = acroForm.getField(
-              QUESTION_BERAKNAT_NEDKOMSTDATUM_CERTIFIER_DOCTOR_FIELD_ID);
-          certifierDoctor.setValue(CHECKED_BOX_VALUE);
-          break;
-        case MIDWIFE:
-          final var certifierMidwife = acroForm.getField(
-              QUESTION_BERAKNAT_NEDKOMSTDATUM_CERTIFIER_MIDWIFE_FIELD_ID);
-          certifierMidwife.setValue(CHECKED_BOX_VALUE);
-          break;
-        case NURSE:
-          final var certifierNurse = acroForm.getField(
-              QUESTION_BERAKNAT_NEDKOMSTDATUM_CERTIFIER_NURSE_FIELD_ID);
-          certifierNurse.setValue(CHECKED_BOX_VALUE);
-          break;
-        default:
-          break;
-      }
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set issuer role");
+    switch (role) {
+      case DOCTOR:
+        final var certifierDoctor = acroForm.getField(
+            QUESTION_BERAKNAT_NEDKOMSTDATUM_CERTIFIER_DOCTOR_FIELD_ID);
+        certifierDoctor.setValue(CHECKED_BOX_VALUE);
+        break;
+      case MIDWIFE:
+        final var certifierMidwife = acroForm.getField(
+            QUESTION_BERAKNAT_NEDKOMSTDATUM_CERTIFIER_MIDWIFE_FIELD_ID);
+        certifierMidwife.setValue(CHECKED_BOX_VALUE);
+        break;
+      case NURSE:
+        final var certifierNurse = acroForm.getField(
+            QUESTION_BERAKNAT_NEDKOMSTDATUM_CERTIFIER_NURSE_FIELD_ID);
+        certifierNurse.setValue(CHECKED_BOX_VALUE);
+        break;
+      default:
+        break;
     }
   }
 
-  private void setSignedDate(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var signedDate = acroForm.getField(SIGNATURE_DATE_FIELD_ID);
-      signedDate.setValue(certificate.signed().format(DateTimeFormatter.ISO_DATE));
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set signed date");
+  private void setSignedDate(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var signedDate = acroForm.getField(SIGNATURE_DATE_FIELD_ID);
+    signedDate.setValue(certificate.signed().format(DateTimeFormatter.ISO_DATE));
+  }
+
+  private void setDigitalSignatureText(PDDocument fk7211Pdf, PDPage page) throws IOException {
+    final var contentStream = new PDPageContentStream(fk7211Pdf, page, AppendMode.APPEND,
+        true, true);
+    contentStream.beginText();
+    contentStream.newLineAtOffset(SIGNATURE_SIGNATURE_POSITION_X, SIGNATURE_SIGNATURE_POSITION_Y);
+    contentStream.setNonStrokingColor(Color.gray);
+    contentStream.setFont(new PDType1Font(FontName.HELVETICA_BOLD), 8);
+    contentStream.showText(DIGITALLY_SIGNED_TEXT);
+    contentStream.endText();
+    contentStream.close();
+  }
+
+  private void setIssuerFullName(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var signatureFullName = acroForm.getField(SIGNATURE_FULL_NAME_FIELD_ID);
+    signatureFullName.setValue(certificate.certificateMetaData().issuer().name().fullName());
+  }
+
+  private void setPaTitles(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var paTitles = certificate.certificateMetaData().issuer().paTitles();
+    if (paTitles != null) {
+
+      final var paTitleCodes = paTitles.stream().map(PaTitle::code)
+          .collect(Collectors.joining(", "));
+
+      final var signaturePaTitles = acroForm.getField(SIGNATURE_PA_TITLE_FIELD_ID);
+      signaturePaTitles.setValue(paTitleCodes);
     }
   }
 
-  private void setDigitalSignatureText(PDDocument fk7211Pdf, PDPage page) {
-    try {
-      final var contentStream = new PDPageContentStream(fk7211Pdf, page, AppendMode.APPEND,
-          true, true);
-      contentStream.beginText();
-      contentStream.newLineAtOffset(SIGNATURE_SIGNATURE_POSITION_X, SIGNATURE_SIGNATURE_POSITION_Y);
-      contentStream.setNonStrokingColor(Color.gray);
-      contentStream.setFont(new PDType1Font(FontName.HELVETICA_BOLD), 8);
-      contentStream.showText(DIGITALLY_SIGNED_TEXT);
-      contentStream.endText();
-      contentStream.close();
+  private void setSpeciality(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var specialities = certificate.certificateMetaData().issuer().specialities();
+    if (specialities != null) {
 
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set digital signature text ");
+      final var mappedSpecialities = specialities.stream().map(Speciality::value)
+          .collect(Collectors.joining(", "));
+
+      final var signaturePaTitles = acroForm.getField(SIGNATURE_SPECIALITY_FIELD_ID);
+      signaturePaTitles.setValue(mappedSpecialities);
     }
   }
 
-  private void setIssuerFullName(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var signatureFullName = acroForm.getField(SIGNATURE_FULL_NAME_FIELD_ID);
-      signatureFullName.setValue(certificate.certificateMetaData().issuer().name().fullName());
-
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set issuer name");
+  private void setHsaId(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var hsaId = certificate.certificateMetaData().issuer().hsaId().id();
+    if (hsaId != null) {
+      final var signatureHsaId = acroForm.getField(SIGNATURE_HSA_ID_FIELD_ID);
+      signatureHsaId.setValue(hsaId);
     }
   }
 
-  private void setPaTitles(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var paTitles = certificate.certificateMetaData().issuer().paTitles();
-      if (paTitles != null) {
-
-        final var paTitleCodes = paTitles.stream().map(PaTitle::code)
-            .collect(Collectors.joining(", "));
-
-        final var signaturePaTitles = acroForm.getField(SIGNATURE_PA_TITLE_FIELD_ID);
-        signaturePaTitles.setValue(paTitleCodes);
-      }
-
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set pa-titles");
+  private void setWorkplaceCode(PDAcroForm acroForm, Certificate certificate) throws IOException {
+    final var workplaceCode = certificate.certificateMetaData().issuingUnit().workplaceCode()
+        .code();
+    if (workplaceCode != null) {
+      final var signatureWorkplaceCode = acroForm.getField(SIGNATURE_WORKPLACE_CODE_FIELD_ID);
+      signatureWorkplaceCode.setValue(workplaceCode);
     }
   }
 
-  private void setSpeciality(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var specialities = certificate.certificateMetaData().issuer().specialities();
-      if (specialities != null) {
+  private void setContactInformation(PDAcroForm acroForm, Certificate certificate)
+      throws IOException {
+    final var unitName = certificate.certificateMetaData().issuingUnit().name().name();
+    final var address = certificate.certificateMetaData().issuingUnit().address().address();
+    final var zipCode = certificate.certificateMetaData().issuingUnit().address().zipCode();
+    final var city = certificate.certificateMetaData().issuingUnit().address().city();
+    final var phoneNumber = certificate.certificateMetaData().issuingUnit().contactInfo()
+        .phoneNumber();
 
-        final var mappedSpecialities = specialities.stream().map(Speciality::value)
-            .collect(Collectors.joining(", "));
+    final var contactInfo =
+        unitName + "\n"
+            + address + "\n"
+            + zipCode + " " + city + "\n"
+            + "Telefon: " + phoneNumber;
 
-        final var signaturePaTitles = acroForm.getField(SIGNATURE_SPECIALITY_FIELD_ID);
-        signaturePaTitles.setValue(mappedSpecialities);
-      }
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set speciality");
-    }
+    final var contactInformation = acroForm.getField(
+        SIGNATURE_CARE_UNIT_CONTACT_INFORMATION_FIELD_ID);
+    contactInformation.setValue(contactInfo);
   }
 
-  private void setHsaId(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var hsaId = certificate.certificateMetaData().issuer().hsaId().id();
-      if (hsaId != null) {
-        final var signatureHsaId = acroForm.getField(SIGNATURE_HSA_ID_FIELD_ID);
-        signatureHsaId.setValue(hsaId);
-      }
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set HSA-id ");
-    }
+  private void setMarginText(PDDocument fk7211Pdf, PDPage page, Certificate certificate)
+      throws IOException {
+    final var contentStream = new PDPageContentStream(fk7211Pdf, page, AppendMode.APPEND,
+        true, true);
+
+    final var certificateId = certificate.id().id();
+    contentStream.transform(Matrix.getRotateInstance(Math.PI / 2, 607, 280));
+    contentStream.beginText();
+    contentStream.newLineAtOffset(30, 30);
+    contentStream.setNonStrokingColor(Color.black);
+    contentStream.setFont(new PDType1Font(FontName.HELVETICA), 8);
+    contentStream.showText(
+        String.format("Intygsid: %s. Intyget är utskrivet från Webcert", certificateId));
+    contentStream.endText();
+    contentStream.close();
   }
 
-  private void setWorkplaceCode(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var workplaceCode = certificate.certificateMetaData().issuingUnit().workplaceCode()
-          .code();
-      if (workplaceCode != null) {
-        final var signatureWorkplaceCode = acroForm.getField(SIGNATURE_WORKPLACE_CODE_FIELD_ID);
-        signatureWorkplaceCode.setValue(workplaceCode);
-      }
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set workplace code");
-    }
-  }
+  private void setDraftWatermark(PDDocument fk7211Pdf, PDPage page) throws IOException {
+    final var contentStream = new PDPageContentStream(fk7211Pdf, page, AppendMode.APPEND,
+        true, true);
 
-  private void setContactInformation(PDAcroForm acroForm, Certificate certificate) {
-    try {
-      final var unitName = certificate.certificateMetaData().issuingUnit().name().name();
-      final var address = certificate.certificateMetaData().issuingUnit().address().address();
-      final var zipCode = certificate.certificateMetaData().issuingUnit().address().zipCode();
-      final var city = certificate.certificateMetaData().issuingUnit().address().city();
-      final var phoneNumber = certificate.certificateMetaData().issuingUnit().contactInfo()
-          .phoneNumber();
+    final var fontHeight = 105;
+    final var font = new PDType1Font(FontName.HELVETICA);
 
-      final var contactInfo = String.format("%s%n%s%n%s %s%nTelefon: %s", unitName, address,
-          zipCode,
-          city, phoneNumber).replaceAll("\\r\\n|\\r|\\n", "\n");
+    final var width = page.getMediaBox().getWidth();
+    float height = page.getMediaBox().getHeight();
 
-      final var contactInformation = acroForm.getField(
-          SIGNATURE_CARE_UNIT_CONTACT_INFORMATION_FIELD_ID);
-      contactInformation.setValue(contactInfo);
+    final var stringWidth = font.getStringWidth(WATERMARK_DRAFT) / 1000 * fontHeight;
+    final var diagonalLength = (float) Math.sqrt(width * width + height * height);
+    final var angle = (float) Math.atan2(height, width);
+    final var x = (diagonalLength - stringWidth) / 2;
+    final var y = -fontHeight / 4;
 
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set contact information");
-    }
-  }
+    contentStream.transform(Matrix.getRotateInstance(angle, 0, 0));
+    contentStream.setFont(font, fontHeight);
 
-  private void setMarginText(PDDocument fk7211Pdf, PDPage page, Certificate certificate) {
-    try {
-      final var contentStream = new PDPageContentStream(fk7211Pdf, page, AppendMode.APPEND,
-          true, true);
+    PDExtendedGraphicsState gs = new PDExtendedGraphicsState();
+    gs.setNonStrokingAlphaConstant(0.5f);
+    gs.setStrokingAlphaConstant(0.5f);
+    contentStream.setGraphicsStateParameters(gs);
+    contentStream.setNonStrokingColor(Color.gray);
 
-      final var certificateId = certificate.id().id();
-      contentStream.transform(Matrix.getRotateInstance(Math.PI / 2, 607, 280));
-      contentStream.beginText();
-      contentStream.newLineAtOffset(30, 30);
-      contentStream.setNonStrokingColor(Color.black);
-      contentStream.setFont(new PDType1Font(FontName.HELVETICA), 8);
-      contentStream.showText(
-          String.format("Intygsid: %s. Intyget är utskrivet från Webcert", certificateId));
-      contentStream.endText();
-      contentStream.close();
-
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set margin text");
-    }
-  }
-
-  private void setDraftWatermark(PDDocument fk7211Pdf, PDPage page) {
-    try {
-      final var contentStream = new PDPageContentStream(fk7211Pdf, page, AppendMode.APPEND,
-          true, true);
-
-      final var fontHeight = 105;
-      final var font = new PDType1Font(FontName.HELVETICA);
-
-      final var width = page.getMediaBox().getWidth();
-      float height = page.getMediaBox().getHeight();
-
-      final var stringWidth = font.getStringWidth(WATERMARK_DRAFT) / 1000 * fontHeight;
-      final var diagonalLength = (float) Math.sqrt(width * width + height * height);
-      final var angle = (float) Math.atan2(height, width);
-      final var x = (diagonalLength - stringWidth) / 2;
-      final var y = -fontHeight / 4;
-
-      contentStream.transform(Matrix.getRotateInstance(angle, 0, 0));
-      contentStream.setFont(font, fontHeight);
-
-      PDExtendedGraphicsState gs = new PDExtendedGraphicsState();
-      gs.setNonStrokingAlphaConstant(0.5f);
-      gs.setStrokingAlphaConstant(0.5f);
-      contentStream.setGraphicsStateParameters(gs);
-      contentStream.setNonStrokingColor(Color.gray);
-
-      contentStream.beginText();
-      contentStream.newLineAtOffset(x, y);
-      contentStream.showText(WATERMARK_DRAFT);
-      contentStream.endText();
-      contentStream.close();
-
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set draft water mark ");
-    }
+    contentStream.beginText();
+    contentStream.newLineAtOffset(x, y);
+    contentStream.showText(WATERMARK_DRAFT);
+    contentStream.endText();
+    contentStream.close();
   }
 
   private String setFileName(Certificate certificate) {
-    try {
-      final var certificateName = certificate.certificateModel().name();
-      final var timestamp = LocalDateTime.now()
-          .format((DateTimeFormatter.ofPattern("yy-MM-dd_HHmm")));
+    final var certificateName = certificate.certificateModel().name();
+    final var timestamp = LocalDateTime.now()
+        .format((DateTimeFormatter.ofPattern("yy-MM-dd_HHmm")));
 
-      return String.format("%s_%s", certificateName, timestamp)
-          .replace("å", "a")
-          .replace("ä", "a")
-          .replace("ö", "o")
-          .replace(" ", "_")
-          .replace("–", "")
-          .replace("__", "_")
-          .toLowerCase();
-
-    } catch (Exception e) {
-      throw new IllegalStateException("Could not set file name ");
-    }
+    return String.format("%s_%s", certificateName, timestamp)
+        .replace("å", "a")
+        .replace("ä", "a")
+        .replace("ö", "o")
+        .replace(" ", "_")
+        .replace("–", "")
+        .replace("__", "_")
+        .toLowerCase();
   }
 }
