9e06d84c9938e6d31d5ce1fdaa0f6a872343f89e
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-04-02 14:19:19 +0200

    INTYGFV-16481: Midwife should be able to revoke certificate

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/action/model/CertificateActionFactory.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/action/model/CertificateActionFactory.java
@@ -90,7 +90,9 @@ public class CertificateActionFactory {
           .actionRules(
               List.of(
                   new ActionRuleWithinCareUnit(),
-                  new ActionRuleRole(List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.NURSE)),
+                  new ActionRuleRole(
+                      List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.NURSE, Role.MIDWIFE)
+                  ),
                   new ActionRuleStatus(List.of(Status.SIGNED))
               )
           )
