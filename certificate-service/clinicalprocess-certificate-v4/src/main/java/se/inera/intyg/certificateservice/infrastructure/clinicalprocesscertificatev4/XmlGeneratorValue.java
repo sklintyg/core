9b6883c81b57dd7e9c8b2bbdf3359293e47cc0dc
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-04-04 09:21:38 +0200

    INTYGFV-16492: XmlGeneratorValue refactor.

--- certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/XmlGeneratorValue.java
+++ certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/XmlGeneratorValue.java
@@ -7,22 +7,28 @@ import se.inera.intyg.certificateservice.domain.certificate.model.ElementData;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValue;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueDate;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueText;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueUnitContactInformation;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Svar;
 
 public class XmlGeneratorValue {
 
-  Map<Class<? extends ElementValue>, XmlGeneratorElementData> converters = Map.of(
+  private final Map<Class<? extends ElementValue>, XmlGeneratorElementData> converters = Map.of(
       ElementValueDate.class, new XmlGeneratorDate(),
       ElementValueText.class, new XmlGeneratorText()
   );
 
   public List<Svar> generate(List<ElementData> elementData) {
     return elementData.stream()
-        .map(
-            data -> converters
-                .getOrDefault(data.value().getClass(), new XmlGeneratorDefault())
-                .generate(data)
-        )
+        .filter(data -> !(data.value() instanceof ElementValueUnitContactInformation))
+        .map(data -> {
+          final var converter = converters.get(data.value().getClass());
+          if (converter == null) {
+            throw new IllegalStateException(
+                "Converter for '%s' not found".formatted(data.value().getClass())
+            );
+          }
+          return converter.generate(data);
+        })
         .filter(Objects::nonNull)
         .toList();
   }
