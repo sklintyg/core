c86e95268c73d347973cc914e408dda7311ae1e2
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-04-05 11:26:03 +0200

    INTYGFV-16492: Added integration tests for Fk7443

---
+++ certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/fk7433/FK7443Constants.java
@@ -0,0 +1,8 @@
+package se.inera.intyg.certificateservice.integrationtest.fk7433;
+
+public class FK7443Constants {
+
+  public static final String FK7443 = "fk7443";
+  public static final String VERSION = "1.0";
+  public static final String WRONG_VERSION = "wrongVersion";
+}
