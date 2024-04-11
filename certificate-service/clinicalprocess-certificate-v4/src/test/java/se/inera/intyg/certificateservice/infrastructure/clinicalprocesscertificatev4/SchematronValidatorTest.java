0d3f9381a47b88017cee7f96b5b4e29a77ec50e2
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-04-11 08:34:17 +0200

    INTYGFV-16409: Updated tests to better verify SchematronValidator of FK7211 & FK7443.

--- certificate-service/clinicalprocess-certificate-v4/src/test/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/SchematronValidatorTest.java
+++ certificate-service/clinicalprocess-certificate-v4/src/test/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/SchematronValidatorTest.java
@@ -53,28 +53,104 @@ class SchematronValidatorTest {
       assertTrue(schematronValidator.validate(certificate1));
     }
 
-    @Test
-    void shallReturnFalseForInvalidCertificate() {
-      final var element = ElementData.builder()
-          .id(new ElementId("1"))
-          .value(
-              ElementValueDate.builder()
-                  .dateId(new FieldId("1.1"))
-                  .build()
-          ).build();
+    @Nested
+    class QuestionNedkomstdatum {
 
-      final var certificate = TestDataCertificate.fk7211CertificateBuilder()
-          .elementData(List.of(element))
-          .build();
+      @Test
+      void shallReturnFalseIfValueIsNull() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueDate.builder()
+                    .dateId(new FieldId("1.1"))
+                    .build()
+            ).build();
 
-      final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
-      final var xml = generator.generate(certificate);
+        final var certificate = TestDataCertificate.fk7211CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
 
-      final var certificate1 = TestDataCertificate.fk7211CertificateBuilder()
-          .xml(xml)
-          .build();
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
 
-      assertFalse(schematronValidator.validate(certificate1));
+        final var certificate1 = TestDataCertificate.fk7211CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
+
+      @Test
+      void shallReturnFalseIfValueIsBeforeToday() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueDate.builder()
+                    .dateId(new FieldId("1.1"))
+                    .date(LocalDate.now().minusDays(1))
+                    .build()
+            ).build();
+
+        final var certificate = TestDataCertificate.fk7211CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
+
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
+
+        final var certificate1 = TestDataCertificate.fk7211CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
+
+      @Test
+      void shallReturnFalseIfValueIsMoreThanOneYearInTheFuture() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueDate.builder()
+                    .dateId(new FieldId("1.1"))
+                    .date(LocalDate.now().plusYears(1).plusDays(2))
+                    .build()
+            ).build();
+
+        final var certificate = TestDataCertificate.fk7211CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
+
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
+
+        final var certificate1 = TestDataCertificate.fk7211CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
+
+      @Test
+      void shallReturnFalseIfQuestionMissing() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueDate.builder().build()
+            ).build();
+
+        final var certificate = TestDataCertificate.fk7211CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
+
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
+
+        final var certificate1 = TestDataCertificate.fk7211CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
     }
   }
 
@@ -82,7 +158,7 @@ class SchematronValidatorTest {
   class FK7443Validation {
 
     @Test
-    void shallReturnTrueForValidCertificate() {
+    void shallReturnTrueIfAllFieldsHaveValues() {
       final var element = ElementData.builder()
           .id(new ElementId("1"))
           .value(
@@ -106,28 +182,112 @@ class SchematronValidatorTest {
       assertTrue(schematronValidator.validate(certificate1));
     }
 
-    @Test
-    void shallReturnFalseForInvalidCertificate() {
-      final var element = ElementData.builder()
-          .id(new ElementId("1"))
-          .value(
-              ElementValueText.builder()
-                  .textId(new FieldId("1.1"))
-                  .build()
-          ).build();
+    @Nested
+    class QuestionDiagnosEllerSymtom {
 
-      final var certificate = TestDataCertificate.fk443CertificateBuilder()
-          .elementData(List.of(element))
-          .build();
+      @Test
+      void shallReturnFalseIfValueIsNull() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueText.builder()
+                    .textId(new FieldId("1.1"))
+                    .build()
+            ).build();
 
-      final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
-      final var xml = generator.generate(certificate);
+        final var certificate = TestDataCertificate.fk443CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
 
-      final var certificate1 = TestDataCertificate.fk443CertificateBuilder()
-          .xml(xml)
-          .build();
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
 
-      assertFalse(schematronValidator.validate(certificate1));
+        final var certificate1 = TestDataCertificate.fk443CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
+
+      @Test
+      void shallReturnFalseIfValueIsBlank() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueText.builder()
+                    .textId(new FieldId("1.1"))
+                    .text("")
+                    .build()
+            ).build();
+
+        final var certificate = TestDataCertificate.fk443CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
+
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
+
+        final var certificate1 = TestDataCertificate.fk443CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
+
+      @Test
+      void shallReturnFalseIfValueHasLengthGreaterThan318() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(
+                ElementValueText.builder()
+                    .textId(new FieldId("1.1"))
+                    .text(
+                        "awddawadwawdawdawdawdawdadwadwawdadawda"
+                            + "wawddawadwawdawdawdawdawdadwadwawd"
+                            + "adawdawawddawadwawdawdawdawdawdadw"
+                            + "adwawdadawdawawddawadwawdawdawdawd"
+                            + "awdadwadwawdadawdawawddawadwawdawd"
+                            + "awdawdawdadwadwawdadawdawawddawad"
+                            + "wawdawdawdawdawdadwadwawdadawdawawd"
+                            + "dawadwawdawdawdawdawdadwadwawdadawd"
+                            + "awawddawadwawdawdawdawdawdadwadwawdadawdddddd")
+                    .build()
+            ).build();
+
+        final var certificate = TestDataCertificate.fk443CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
+
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
+
+        final var certificate1 = TestDataCertificate.fk443CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
+
+      @Test
+      void shallReturnFalseIfQuestionMissing() {
+        final var element = ElementData.builder()
+            .id(new ElementId("1"))
+            .value(ElementValueText.builder().build())
+            .build();
+
+        final var certificate = TestDataCertificate.fk443CertificateBuilder()
+            .elementData(List.of(element))
+            .build();
+
+        final var generator = new XmlGeneratorCertificateV4(new XmlGeneratorValue());
+        final var xml = generator.generate(certificate);
+
+        final var certificate1 = TestDataCertificate.fk443CertificateBuilder()
+            .xml(xml)
+            .build();
+
+        assertFalse(schematronValidator.validate(certificate1));
+      }
     }
   }
 }
