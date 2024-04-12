71da0dd938b029658256a110847a2da216309c5c
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-04-12 14:53:25 +0200

    INTYGFV-16279: Fixed integration tests

--- certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/fk7433/FK7443ActiveIT.java
+++ certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/fk7433/FK7443ActiveIT.java
@@ -596,7 +596,7 @@ class FK7443ActiveIT {
               .patient(ANONYMA_REACT_ATTILA_DTO)
               .build()
       );
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -646,7 +646,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -674,7 +674,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -704,7 +704,7 @@ class FK7443ActiveIT {
               .build()
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -734,7 +734,7 @@ class FK7443ActiveIT {
               .build()
       );
 
-      final var questionId = "2";
+      final var questionId = "3";
       final var expectedData = List.of(
           CertificateDataValueDateRange.builder()
               .id("HALVA")
@@ -1626,7 +1626,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -1667,7 +1667,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -2136,7 +2136,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar.";
       final var certificate = certificate(testCertificates);
 
@@ -2163,7 +2163,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var expectedText = "Ett nytt exempel på ett svar. Ett nytt exempel på ett svar."
           + "Ett nytt exempel på ett svar.  Ett nytt exempel på ett svar."
           + "Ett nytt exempel på ett svar. Ett nytt exempel på ett svar."
@@ -2209,7 +2209,7 @@ class FK7443ActiveIT {
           defaultTestablilityCertificateRequest(FK7443, VERSION)
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       final var certificate = certificate(testCertificates);
 
       Objects.requireNonNull(
@@ -2331,7 +2331,7 @@ class FK7443ActiveIT {
               .withAddress("")
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       Objects.requireNonNull(
           certificate.getData().put(
               questionId,
@@ -2372,7 +2372,7 @@ class FK7443ActiveIT {
               .withZipCode("")
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       Objects.requireNonNull(
           certificate.getData().put(
               questionId,
@@ -2413,7 +2413,7 @@ class FK7443ActiveIT {
               .withCity("")
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       Objects.requireNonNull(
           certificate.getData().put(
               questionId,
@@ -2454,7 +2454,7 @@ class FK7443ActiveIT {
               .withPhoneNumber("")
       );
 
-      final var questionId = "1";
+      final var questionId = "2";
       Objects.requireNonNull(
           certificate.getData().put(
               questionId,
