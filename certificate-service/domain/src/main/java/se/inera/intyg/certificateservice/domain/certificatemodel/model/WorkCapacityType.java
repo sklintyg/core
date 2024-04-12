37068d34a265e683bdaa066e4ed78fba84874e06
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-04-12 10:48:50 +0200

    INTYGFV-16279: Reformat

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/WorkCapacityType.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/WorkCapacityType.java
@@ -4,17 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum WorkCapacityType {
-    EN_ATTANDEL("12,5 procent"),
-    EN_FJARDEDEL("25 procent"),
-    HALVA("50 procent"),
-    TRE_FJARDEDELAR("75 procent"),
-    HELA("100 procent");
+  EN_ATTANDEL("12,5 procent"),
+  EN_FJARDEDEL("25 procent"),
+  HALVA("50 procent"),
+  TRE_FJARDEDELAR("75 procent"),
+  HELA("100 procent");
 
-    private final String label;
-    public static final String CODE_SYSTEM = "KV_FKMU_0009";
+  private final String label;
+  public static final String CODE_SYSTEM = "KV_FKMU_0009";
 
-    WorkCapacityType(String label) {
-        this.label = label;
-    }
+  WorkCapacityType(String label) {
+    this.label = label;
+  }
 
 }
