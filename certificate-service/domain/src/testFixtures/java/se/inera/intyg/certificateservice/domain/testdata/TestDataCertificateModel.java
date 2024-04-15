989d9dc6549449465cc7f19f01c16c10e8e723eb
mikeyOne <55154460+mikeyOne@users.noreply.github.com>
2024-04-15 11:50:52 +0200

    INTYGFV-16489: Added test for no addition of intygsgivare in xml for 7211. Added compile dependencies for getting rid of warnings during build. (#150)

--- certificate-service/domain/src/testFixtures/java/se/inera/intyg/certificateservice/domain/testdata/TestDataCertificateModel.java
+++ certificate-service/domain/src/testFixtures/java/se/inera/intyg/certificateservice/domain/testdata/TestDataCertificateModel.java
@@ -5,6 +5,11 @@ import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertific
 import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7211_PDF_PATH;
 import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7211_TYPE;
 import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7211_VERSION;
+import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7443_CODE_TYPE;
+import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7443_NAME;
+import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7443_PDF_PATH;
+import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7443_TYPE;
+import static se.inera.intyg.certificateservice.domain.testdata.TestDataCertificateModelConstants.FK7443_VERSION;
 
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModelId;
@@ -24,7 +29,21 @@ public class TestDataCertificateModel {
         .name(FK7211_NAME)
         .type(FK7211_CODE_TYPE)
         .pdfTemplatePath(FK7211_PDF_PATH);
+  }
 
+  public static final CertificateModel FK7443_CERTIFICATE_MODEL = certificateFk7443ModelBuilder().build();
+
+  public static CertificateModel.CertificateModelBuilder certificateFk7443ModelBuilder() {
+    return CertificateModel.builder()
+        .id(
+            CertificateModelId.builder()
+                .type(FK7443_TYPE)
+                .version(FK7443_VERSION)
+                .build()
+        )
+        .name(FK7443_NAME)
+        .type(FK7443_CODE_TYPE)
+        .pdfTemplatePath(FK7443_PDF_PATH);
   }
 
 }
--- certificate-service/domain/src/testFixtures/java/se/inera/intyg/certificateservice/domain/testdata/TestDataCertificateModelConstants.java
+++ certificate-service/domain/src/testFixtures/java/se/inera/intyg/certificateservice/domain/testdata/TestDataCertificateModelConstants.java
@@ -19,4 +19,15 @@ public class TestDataCertificateModelConstants {
   );
   public static final String FK7211_PDF_PATH = "fk7211_v1.pdf";
   public static final String FK7211_NAME = "Intyg om graviditet";
+
+
+  public static final CertificateType FK7443_TYPE = new CertificateType("fk7443");
+  public static final CertificateVersion FK7443_VERSION = new CertificateVersion("1.0");
+  public static final Code FK7443_CODE_TYPE = new Code(
+      "ITFP",
+      "b64ea353-e8f6-4832-b563-fc7d46f29548",
+      "Intyg om tillfällig föräldrapenning"
+  );
+  public static final String FK7443_PDF_PATH = "fk7443_v1.pdf";
+  public static final String FK7443_NAME = "Intyg om tillfällig föräldrapenning";
 }
