5865cef2372710372902de8581dcafb5584a2f96
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2024-05-03 10:12:13 +0200

    INTYGFV-16548: Implementation of CertificateExternalTypeInfo endpoint. (#163)

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/repository/CertificateModelRepository.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/repository/CertificateModelRepository.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModelId;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateType;
+import se.inera.intyg.certificateservice.domain.common.model.Code;
 
 public interface CertificateModelRepository {
 
   List<CertificateModel> findAllActive();
 
   Optional<CertificateModel> findLatestActiveByType(CertificateType certificateType);
+
+  Optional<CertificateModel> findLatestActiveByExternalType(Code code);
 
   CertificateModel getById(CertificateModelId certificateModelId);
 }
