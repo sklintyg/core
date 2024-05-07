397f8d01c92bebca5fcb5527509c126277623062
Mikaela Romild <mikaela.romild@soprasteria.com>
2024-05-07 16:42:55 +0200

    INTYGFV-16574: Continued on conversion

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/citizen/service/converter/CitizenCertificateMetadataConverter.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/citizen/service/converter/CitizenCertificateMetadataConverter.java
@@ -1,13 +1,18 @@
 package se.inera.intyg.certificateservice.application.citizen.service.converter;
 
+import java.util.List;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateMetadataDTO;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateRecipientDTO;
+import se.inera.intyg.certificateservice.application.certificate.dto.CertificateRelationDTO;
+import se.inera.intyg.certificateservice.application.certificate.dto.CertificateRelationTypeDTO;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateRelationsDTO;
+import se.inera.intyg.certificateservice.application.certificate.dto.CertificateStatusTypeDTO;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateSummaryDTO;
 import se.inera.intyg.certificateservice.application.certificate.dto.StaffDTO;
 import se.inera.intyg.certificateservice.application.certificate.dto.UnitDTO;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
+import se.inera.intyg.certificateservice.domain.staff.model.Staff;
 import se.inera.intyg.certificateservice.domain.unit.model.CareUnit;
 import se.inera.intyg.certificateservice.domain.unit.model.IssuingUnit;
 
@@ -21,12 +26,12 @@ public class CitizenCertificateMetadataConverter {
         .typeVersion(certificate.certificateModel().id().version().version())
         .typeName(certificate.certificateModel().type().code())
         .created(certificate.created())
-        .issuedBy(convertStaff())
+        .issuedBy(convertStaff(certificate.certificateMetaData().issuer()))
         .unit(convertUnit(certificate.certificateMetaData().issuingUnit()))
         .careUnit(convertCareUnit(certificate.certificateMetaData().careUnit()))
-        .recipient(convertRecipient())
-        .summary(convertSummary())
-        .relations(convertRelations())
+        .recipient(convertRecipient(certificate))
+        .summary(convertSummary(certificate))
+        .relations(convertRelations(certificate))
         .build();
   }
 
@@ -56,22 +61,43 @@ public class CitizenCertificateMetadataConverter {
         .build();
   }
 
-  private StaffDTO convertStaff() {
-    return StaffDTO.builder().build();
+  private StaffDTO convertStaff(Staff staff) {
+    return StaffDTO.builder()
+        .fullName(staff.name().fullName())
+        .build();
   }
 
-  private CertificateRecipientDTO convertRecipient() {
-    return CertificateRecipientDTO.builder().build();
+  private CertificateRecipientDTO convertRecipient(Certificate certificate) {
+    return CertificateRecipientDTO.builder()
+        .id(certificate.certificateModel().recipient().id().id())
+        .name(certificate.certificateModel().recipient().name())
+        .sent(certificate.sent().sentAt())
+        .build();
   }
 
   private CertificateSummaryDTO convertSummary(Certificate certificate) {
     return CertificateSummaryDTO.builder()
-        .label()
-        .value()
+        //TODO kolla upp hur vi får ut value och vart vi ska sätta label
         .build();
   }
 
-  private CertificateRelationsDTO convertRelations() {
-    return CertificateRelationsDTO.builder().build();
+  private CertificateRelationsDTO convertRelations(Certificate certificate) {
+    return CertificateRelationsDTO.builder()
+        .parent(convertParent(certificate))
+        .children(convertChildren(certificate))
+        .build();
+  }
+
+  private CertificateRelationDTO convertParent(Certificate certificate) {
+    return CertificateRelationDTO.builder()
+        .certificateId(certificate.parent().certificateId().id())
+        .created(certificate.parent().created())
+        .type(CertificateRelationTypeDTO.valueOf(certificate.parent().type().name()))
+        .status(CertificateStatusTypeDTO.valueOf(certificate.parent().status().name()))
+        .build();
+  }
+
+  private List<CertificateRelationDTO> convertChildren(Certificate certificate) {
+    return null;
   }
 }
