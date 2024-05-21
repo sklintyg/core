20417094682b5ca0ad44c14edeceea8f1da80aa3
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2024-05-21 09:37:12 +0200

    INTYGFV-16592: Added integration test for receiving complements

--- certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/util/ApiUtil.java
+++ certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/util/ApiUtil.java
@@ -48,6 +48,7 @@ import se.inera.intyg.certificateservice.application.citizen.dto.GetCitizenCerti
 import se.inera.intyg.certificateservice.application.citizen.dto.GetCitizenCertificateResponse;
 import se.inera.intyg.certificateservice.application.citizen.dto.PrintCitizenCertificateRequest;
 import se.inera.intyg.certificateservice.application.citizen.dto.PrintCitizenCertificateResponse;
+import se.inera.intyg.certificateservice.application.message.dto.IncomingMessageRequest;
 import se.inera.intyg.certificateservice.application.patient.dto.GetPatientCertificatesRequest;
 import se.inera.intyg.certificateservice.application.patient.dto.GetPatientCertificatesResponse;
 import se.inera.intyg.certificateservice.application.unit.dto.GetUnitCertificatesInfoRequest;
@@ -393,6 +394,22 @@ public class ApiUtil {
         port,
         certificateId
     );
+
+    final var headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    return this.restTemplate.exchange(
+        requestUrl,
+        HttpMethod.POST,
+        new HttpEntity<>(request, headers),
+        new ParameterizedTypeReference<>() {
+        },
+        Collections.emptyMap()
+    );
+  }
+
+  public ResponseEntity<Void> receiveMessage(IncomingMessageRequest request) {
+    final var requestUrl = "http://localhost:%s/api/message".formatted(port);
 
     final var headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
