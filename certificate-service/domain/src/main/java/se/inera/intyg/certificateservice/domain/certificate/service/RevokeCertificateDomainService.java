9fb7b6abc875a087b9c9489f79cf48e055de5c72
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2024-05-23 12:05:58 +0200

    INTYGFV-16594: Set messages as handled when revoking a certificate

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificate/service/RevokeCertificateDomainService.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificate/service/RevokeCertificateDomainService.java
@@ -14,12 +14,14 @@ import se.inera.intyg.certificateservice.domain.common.model.RevokedInformation;
 import se.inera.intyg.certificateservice.domain.event.model.CertificateEvent;
 import se.inera.intyg.certificateservice.domain.event.model.CertificateEventType;
 import se.inera.intyg.certificateservice.domain.event.service.CertificateEventDomainService;
+import se.inera.intyg.certificateservice.domain.message.service.SetMessagesToHandleDomainService;
 
 @RequiredArgsConstructor
 public class RevokeCertificateDomainService {
 
   private final CertificateRepository certificateRepository;
   private final CertificateEventDomainService certificateEventDomainService;
+  private final SetMessagesToHandleDomainService setMessagesToHandleDomainService;
 
   public Certificate revoke(CertificateId certificateId, ActionEvaluation actionEvaluation,
       RevokedInformation revokedInformation) {
@@ -36,6 +38,8 @@ public class RevokeCertificateDomainService {
     certificate.revoke(actionEvaluation, revokedInformation);
 
     final var revokedCertificate = certificateRepository.save(certificate);
+
+    setMessagesToHandleDomainService.handle(revokedCertificate.messages());
 
     certificateEventDomainService.publish(
         CertificateEvent.builder()
