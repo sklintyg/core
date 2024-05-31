3305b13c18f972cffec4d78a901cc8455e0bbba6
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2024-05-31 10:05:00 +0200

    INTYGFV-16280: Added internal api to fetch questions

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/message/MessageInternalApiController.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/message/MessageInternalApiController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.inera.intyg.certificateservice.application.message.dto.GetCertificateMessageInternalResponse;
 import se.inera.intyg.certificateservice.application.message.dto.GetMessageInternalXmlResponse;
+import se.inera.intyg.certificateservice.application.message.service.GetCertificateMessageInternalService;
 import se.inera.intyg.certificateservice.application.message.service.GetMessageInternalXmlService;
 
 @RequiredArgsConstructor
@@ -14,9 +16,16 @@ import se.inera.intyg.certificateservice.application.message.service.GetMessageI
 public class MessageInternalApiController {
 
   private final GetMessageInternalXmlService getMessageInternalXmlService;
+  private final GetCertificateMessageInternalService getCertificateMessageInternalService;
 
   @PostMapping("/{messageId}/xml")
   GetMessageInternalXmlResponse getMessageXml(@PathVariable("messageId") String messageId) {
     return getMessageInternalXmlService.get(messageId);
+  }
+
+  @PostMapping("/{certificateId}")
+  GetCertificateMessageInternalResponse getMessagesForCertificate(
+      @PathVariable("certificateId") String certificateId) {
+    return getCertificateMessageInternalService.get(certificateId);
   }
 }
---
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/message/service/GetCertificateMessageInternalService.java
@@ -0,0 +1,33 @@
+package se.inera.intyg.certificateservice.application.message.service;
+
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se.inera.intyg.certificateservice.application.message.dto.GetCertificateMessageInternalResponse;
+import se.inera.intyg.certificateservice.application.message.service.converter.QuestionConverter;
+import se.inera.intyg.certificateservice.domain.certificate.model.CertificateId;
+import se.inera.intyg.certificateservice.domain.certificate.repository.CertificateRepository;
+
+@Service
+@RequiredArgsConstructor
+public class GetCertificateMessageInternalService {
+
+  private final CertificateRepository certificateRepository;
+  private final QuestionConverter questionConverter;
+
+  public GetCertificateMessageInternalResponse get(String certificateId) {
+    final var certificate = certificateRepository.getById(new CertificateId(certificateId));
+    return GetCertificateMessageInternalResponse.builder()
+        .questions(
+            certificate.messages().stream()
+                .map(message ->
+                    questionConverter.convert(
+                        message,
+                        Collections.emptyList()
+                    )
+                )
+                .toList()
+        )
+        .build();
+  }
+}
