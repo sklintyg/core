02e0fb9fbafcb97ef780176137547056f688b8f8
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-06-28 14:29:41 +0200

    INTYGFV-16686: Utility class constructor.

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk3226/CodeSystemIcd10Se.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk3226/CodeSystemIcd10Se.java
@@ -4,6 +4,10 @@ import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementDi
 
 public class CodeSystemIcd10Se {
 
+  private CodeSystemIcd10Se() {
+    throw new IllegalStateException("Utility class");
+  }
+
   public static final String ICD_10_SE_CODE_SYSTEM = "1.2.752.116.1.1.1.1.8";
   public static final String DIAGNOS_ICD_10_ID = "ICD_10_SE";
   private static final String DIAGNOS_ICD_10_LABEL = "ICD-10-SE";
