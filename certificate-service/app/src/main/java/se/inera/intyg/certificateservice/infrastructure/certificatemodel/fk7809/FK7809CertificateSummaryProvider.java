b4d51cbcbe63e290a7f94ebc3985e704c5cbbe4b
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-07-01 15:49:59 +0200

    INTYGFV-16690: Fixed summary provider

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7809/FK7809CertificateSummaryProvider.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7809/FK7809CertificateSummaryProvider.java
@@ -1,8 +1,12 @@
 package se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7809;
 
+import static se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7809.CertificateModelFactoryFK7809.DIAGNOSIS_ID;
+import static se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7809.CertificateModelFactoryFK7809.DIAGNOS_1;
+
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateSummary;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateSummaryProvider;
+import se.inera.intyg.certificateservice.infrastructure.certificatemodel.common.DiagnosisSummaryValue;
 
 public class FK7809CertificateSummaryProvider implements CertificateSummaryProvider {
 
@@ -10,7 +14,10 @@ public class FK7809CertificateSummaryProvider implements CertificateSummaryProvi
   public CertificateSummary summaryOf(Certificate certificate) {
     return CertificateSummary.builder()
         .label("Avser diagnos")
-        .value("Namn på exempeldiagnos")
+        .value(
+            certificate.signed() != null
+                ? DiagnosisSummaryValue.value(DIAGNOSIS_ID, DIAGNOS_1, certificate)
+                : "")
         .build();
   }
 }
