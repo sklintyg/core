37517d69787cb3edfb8ce055bdca794c14656854
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-08-16 09:03:29 +0200

    INTYGFV-16762: Remove ToString-annotation.

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/unit/model/UnitStatistics.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/unit/model/UnitStatistics.java
@@ -3,12 +3,10 @@ package se.inera.intyg.certificateservice.domain.unit.model;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Setter
 @Getter
 @EqualsAndHashCode
-@ToString
 public class UnitStatistics {
 
   private long certificateCount;
