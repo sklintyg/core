dca0e2174715e0863ee6d35c19b4dcd4d1ddffa7
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-10-09 14:37:23 +0200

    K1J-384: Added svarPa to xml if complemented

--- certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/certificate/XmlGeneratorCertificateV4.java
+++ certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/certificate/XmlGeneratorCertificateV4.java
@@ -2,17 +2,22 @@ package se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertific
 
 import jakarta.xml.bind.JAXBContext;
 import java.io.StringWriter;
+import java.util.Comparator;
 import lombok.RequiredArgsConstructor;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
+import se.inera.intyg.certificateservice.domain.certificate.model.RelationType;
 import se.inera.intyg.certificateservice.domain.certificate.model.Signature;
 import se.inera.intyg.certificateservice.domain.certificate.model.Xml;
 import se.inera.intyg.certificateservice.domain.certificate.service.XmlGenerator;
+import se.inera.intyg.certificateservice.domain.message.model.Message;
+import se.inera.intyg.certificateservice.domain.message.model.MessageType;
 import se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertificatev4.common.XmlGeneratorIntyg;
 import se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertificatev4.validation.XmlValidationService;
 import se.riv.clinicalprocess.healthcond.certificate.registerCertificate.v3.ObjectFactory;
 import se.riv.clinicalprocess.healthcond.certificate.registerCertificate.v3.RegisterCertificateType;
 import se.riv.clinicalprocess.healthcond.certificate.types.v3.DatePeriodType;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Intyg;
+import se.riv.clinicalprocess.healthcond.certificate.v3.MeddelandeReferens;
 
 @RequiredArgsConstructor
 public class XmlGeneratorCertificateV4 implements XmlGenerator {
@@ -34,6 +39,7 @@ public class XmlGeneratorCertificateV4 implements XmlGenerator {
   private Xml generate(Certificate certificate, Signature signature, boolean validate) {
     final var xml = marshall(
         registerCertificateType(
+            svarPa(certificate),
             intyg(
                 certificate,
                 signature
@@ -52,14 +58,38 @@ public class XmlGeneratorCertificateV4 implements XmlGenerator {
     return xml;
   }
 
-  private static RegisterCertificateType registerCertificateType(Intyg intyg) {
+  private static RegisterCertificateType registerCertificateType(MeddelandeReferens svarPa,
+      Intyg intyg) {
     final var registerCertificateType = new RegisterCertificateType();
+    registerCertificateType.setSvarPa(svarPa);
     registerCertificateType.setIntyg(intyg);
     return registerCertificateType;
   }
 
   private Intyg intyg(Certificate certificate, Signature signature) {
     return XmlGeneratorIntyg.intyg(certificate, signature, xmlGeneratorValue);
+  }
+
+  private MeddelandeReferens svarPa(Certificate certificate) {
+    if (certificate.parent() != null && certificate.parent().type() == RelationType.COMPLEMENT) {
+      final var svarPa = new MeddelandeReferens();
+      final var unhandledComplements = certificate.messages(MessageType.COMPLEMENT)
+          .stream()
+          .filter(message -> !message.isHandled())
+          .sorted(Comparator.comparing(Message::sent))
+          .toList();
+
+      if (unhandledComplements.isEmpty()) {
+        return null;
+      }
+
+      final var complement = unhandledComplements.get(unhandledComplements.size() - 1);
+      svarPa.setMeddelandeId(complement.id().id());
+      svarPa.setReferensId(complement.reference().reference());
+      return svarPa;
+    }
+
+    return null;
   }
 
   private static Xml marshall(RegisterCertificateType registerCertificateType) {
