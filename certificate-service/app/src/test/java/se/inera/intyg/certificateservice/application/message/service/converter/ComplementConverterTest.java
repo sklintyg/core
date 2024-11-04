930f8b0e00a162284a068363d039506d134c1699
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2024-11-04 05:31:11 +0100

    K1J-481: Handle situation if complement question includes element id missing from certificate type. This should not cause problem opening the certificate

--- certificate-service/app/src/test/java/se/inera/intyg/certificateservice/application/message/service/converter/ComplementConverterTest.java
+++ certificate-service/app/src/test/java/se/inera/intyg/certificateservice/application/message/service/converter/ComplementConverterTest.java
@@ -4,21 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfigurationDate;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementId;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
-import se.inera.intyg.certificateservice.domain.certificatemodel.model.FieldId;
 import se.inera.intyg.certificateservice.domain.message.model.Complement;
 import se.inera.intyg.certificateservice.domain.message.model.Content;
 
 class ComplementConverterTest {
 
   private static final ElementId ELEMENT_SPECIFICATION_ID = new ElementId("elementSpecificationId");
+  private static final ElementId MISSING_ELEMENT_SPECIFICATION_ID = new ElementId(
+      "missingElementSpecificationId");
   private static final String QUESTION_TEXT = "questionText";
-  private static final FieldId FIELD_ID = new FieldId("code");
   private static final Content CONTENT = new Content("content");
   private ComplementConverter complementConverter;
   private Complement complement;
@@ -68,5 +69,35 @@ class ComplementConverterTest {
   void shallIncludeMessage() {
     assertEquals(CONTENT.content(),
         complementConverter.convert(complement, certificate).getMessage());
+  }
+
+  @Nested
+  class ComplementingQuestionIdThatIsMissing {
+
+    @BeforeEach
+    void setUp() {
+      complement = Complement.builder()
+          .elementId(MISSING_ELEMENT_SPECIFICATION_ID)
+          .content(CONTENT)
+          .build();
+    }
+
+    @Test
+    void shallIncludeQuestionId() {
+      assertEquals(MISSING_ELEMENT_SPECIFICATION_ID.id(),
+          complementConverter.convert(complement, certificate).getQuestionId());
+    }
+
+    @Test
+    void shallIncludeQuestionText() {
+      assertEquals(MISSING_ELEMENT_SPECIFICATION_ID.id(),
+          complementConverter.convert(complement, certificate).getQuestionText());
+    }
+
+    @Test
+    void shallIncludeMessage() {
+      assertEquals(CONTENT.content(),
+          complementConverter.convert(complement, certificate).getMessage());
+    }
   }
 }
