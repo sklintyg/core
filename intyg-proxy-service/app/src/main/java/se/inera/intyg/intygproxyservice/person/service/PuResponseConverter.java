fd2299d8eb9b76510eaf79bd7b61534bacdc02d7
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-11-22 13:26:54 +0100

    K1J-520: Fixed converter

--- intyg-proxy-service/app/src/main/java/se/inera/intyg/intygproxyservice/person/service/PuResponseConverter.java
+++ intyg-proxy-service/app/src/main/java/se/inera/intyg/intygproxyservice/person/service/PuResponseConverter.java
@@ -2,7 +2,9 @@ package se.inera.intyg.intygproxyservice.person.service;
 
 import static se.inera.intyg.intygproxyservice.integration.api.pu.Status.FOUND;
 
+import se.inera.intyg.intygproxyservice.integration.api.pu.Person;
 import se.inera.intyg.intygproxyservice.integration.api.pu.PuResponse;
+import se.inera.intyg.intygproxyservice.integration.api.pu.Status;
 import se.inera.intyg.intygproxyservice.person.dto.PersonDTO;
 import se.inera.intyg.intygproxyservice.person.dto.PersonResponse;
 import se.inera.intyg.intygproxyservice.person.dto.StatusDTOType;
@@ -16,13 +18,21 @@ public class PuResponseConverter {
   public static PersonResponse convert(PersonDTOMapper mapper, PuResponse puResponse) {
     return PersonResponse.builder()
         .person(
-            FOUND.equals(puResponse.getStatus())
-                ? mapper.toDTO(puResponse.getPerson())
-                : PersonDTO.builder().personnummer(puResponse.getPerson().getPersonnummer()).build()
+            getPerson(puResponse.getPerson(), puResponse.getStatus(), mapper)
         )
         .status(
             StatusDTOType.valueOf(puResponse.getStatus().name())
         )
         .build();
+  }
+
+  private static PersonDTO getPerson(Person person, Status status, PersonDTOMapper mapper) {
+    if (person == null) {
+      return null;
+    }
+
+    return FOUND.equals(status)
+        ? mapper.toDTO(person)
+        : PersonDTO.builder().personnummer(person.getPersonnummer()).build();
   }
 }
