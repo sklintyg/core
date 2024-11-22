4ca51e0821041342bc658f150d2eef7209d67c2f
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-11-22 13:44:30 +0100

    K1J-520: Removed batch service and done batching in integration service

--- intyg-proxy-service/integration-pu-v5/src/main/java/se/inera/intyg/intygproxyservice/integration/pu/v5/PuIntegrationServiceV5.java
+++ intyg-proxy-service/integration-pu-v5/src/main/java/se/inera/intyg/intygproxyservice/integration/pu/v5/PuIntegrationServiceV5.java
@@ -1,26 +1,45 @@
 package se.inera.intyg.intygproxyservice.integration.pu.v5;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.intygproxyservice.integration.api.pu.PuPersonsRequest;
 import se.inera.intyg.intygproxyservice.integration.api.pu.PuPersonsResponse;
 import se.inera.intyg.intygproxyservice.integration.api.pu.PuRequest;
 import se.inera.intyg.intygproxyservice.integration.api.pu.PuResponse;
+import se.inera.intyg.intygproxyservice.integration.api.pu.PuService;
+import se.inera.intyg.intygproxyservice.integration.pu.v5.batch.BatchUtil;
 import se.inera.intyg.intygproxyservice.integration.pu.v5.client.PuClientV5;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class PuIntegrationServiceV5 {
+public class PuIntegrationServiceV5 implements PuService {
 
   private final PuClientV5 puClientV5;
 
+  @Value("${integration.pu.batch.size}")
+  private int batchSize;
+
+  @Override
   public PuResponse findPerson(PuRequest puRequest) {
     return puClientV5.findPerson(puRequest);
   }
 
+  @Override
   public PuPersonsResponse findPersons(PuPersonsRequest puRequest) {
-    return puClientV5.findPersons(puRequest);
+    final var batches = BatchUtil.split(puRequest.getPersonIds(), batchSize);
+    return PuPersonsResponse.builder()
+        .persons(
+            batches.stream()
+                .map(batch -> PuPersonsRequest.builder().personIds(batch).build())
+                .map(puClientV5::findPersons)
+                .map(PuPersonsResponse::getPersons)
+                .flatMap(List::stream)
+                .toList()
+        )
+        .build();
   }
 }
--- intyg-proxy-service/integration-pu-v5/src/test/java/se/inera/intyg/intygproxyservice/integration/pu/v5/PuIntegrationServiceV5Test.java
+++ intyg-proxy-service/integration-pu-v5/src/test/java/se/inera/intyg/intygproxyservice/integration/pu/v5/PuIntegrationServiceV5Test.java
@@ -1,14 +1,22 @@
 package se.inera.intyg.intygproxyservice.integration.pu.v5;
 
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import se.inera.intyg.intygproxyservice.integration.api.pu.Person;
 import se.inera.intyg.intygproxyservice.integration.api.pu.PuPersonsRequest;
-import se.inera.intyg.intygproxyservice.integration.api.pu.PuRequest;
+import se.inera.intyg.intygproxyservice.integration.api.pu.PuPersonsResponse;
+import se.inera.intyg.intygproxyservice.integration.api.pu.PuResponse;
 import se.inera.intyg.intygproxyservice.integration.pu.v5.client.PuClientV5;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,20 +28,60 @@ class PuIntegrationServiceV5Test {
   @InjectMocks
   private PuIntegrationServiceV5 puIntegrationServiceV5;
 
-  @Test
-  void shouldCallClientForGetPerson() {
-    final var request = PuRequest.builder().build();
-    puIntegrationServiceV5.findPerson(request);
+  private static final PuPersonsResponse RESPONSE_B1 = PuPersonsResponse.builder()
+      .persons(List.of(
+              PuResponse.found(Person.builder().personnummer("1").build()),
+              PuResponse.found(Person.builder().personnummer("2").build()),
+              PuResponse.found(Person.builder().personnummer("3").build()),
+              PuResponse.found(Person.builder().personnummer("4").build()),
+              PuResponse.found(Person.builder().personnummer("5").build())
+          )
+      )
+      .build();
 
-    verify(puClient).findPerson(request);
+  private static final PuPersonsResponse RESPONSE_B2 = PuPersonsResponse.builder()
+      .persons(List.of(
+              PuResponse.found(Person.builder().personnummer("6").build()),
+              PuResponse.found(Person.builder().personnummer("7").build()),
+              PuResponse.found(Person.builder().personnummer("8").build()),
+              PuResponse.found(Person.builder().personnummer("9").build())
+          )
+      )
+      .build();
+
+  @BeforeEach
+  void setup() {
+    ReflectionTestUtils.setField(puIntegrationServiceV5, "batchSize", 5);
+    when(puClient.findPersons(
+        PuPersonsRequest.builder()
+            .personIds(List.of("1", "2", "3", "4", "5"))
+            .build()
+    )).thenReturn(
+        RESPONSE_B1
+    );
+
+    when(puClient.findPersons(
+        PuPersonsRequest.builder()
+            .personIds(List.of("6", "7", "8", "9"))
+            .build()
+    )).thenReturn(
+        RESPONSE_B2
+    );
   }
 
   @Test
-  void shouldCallClientForGetPersons() {
-    final var request = PuPersonsRequest.builder().build();
-    puIntegrationServiceV5.findPersons(request);
+  void shouldMakeOneCallForEachBatchAndMergeResults() {
+    final var request = PuPersonsRequest.builder()
+        .personIds(List.of("1", "2", "3", "4", "5", "6", "7", "8", "9"))
+        .build();
 
-    verify(puClient).findPersons(request);
+    final var response = puIntegrationServiceV5.findPersons(request);
+
+    assertAll(
+        () -> assertEquals(9, response.getPersons().size()),
+        () -> assertTrue(response.getPersons().containsAll(RESPONSE_B1.getPersons())),
+        () -> assertTrue(response.getPersons().containsAll(RESPONSE_B2.getPersons()))
+    );
   }
 
 }
