1b5615672472b4c8d7ac158422216a39c6549200
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-12-05 20:52:19 +0100

    K1J-560: Added disable validation

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/dto/validation/CertificateDataValidationType.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/dto/validation/CertificateDataValidationType.java
@@ -1,5 +1,5 @@
 package se.inera.intyg.certificateservice.application.certificate.dto.validation;
 
 public enum CertificateDataValidationType {
-  MANDATORY_VALIDATION, SHOW_VALIDATION, TEXT_VALIDATION
+  MANDATORY_VALIDATION, SHOW_VALIDATION, TEXT_VALIDATION, DISABLE_VALIDATION
 }
