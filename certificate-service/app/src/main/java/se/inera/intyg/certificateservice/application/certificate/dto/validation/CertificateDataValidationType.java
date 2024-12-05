9135081b5d721307300eadf79225ebe18119bea5
mhornfeldt <michaela.hoernfeldt@soprasteria.com>
2024-12-05 21:02:07 +0100

    K1J-560: Added disable sub elements validation

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/dto/validation/CertificateDataValidationType.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/dto/validation/CertificateDataValidationType.java
@@ -1,5 +1,5 @@
 package se.inera.intyg.certificateservice.application.certificate.dto.validation;
 
 public enum CertificateDataValidationType {
-  MANDATORY_VALIDATION, SHOW_VALIDATION, TEXT_VALIDATION, DISABLE_VALIDATION
+  MANDATORY_VALIDATION, SHOW_VALIDATION, TEXT_VALIDATION, DISABLE_VALIDATION, DISABLE_SUB_ELEMENT_VALIDATION
 }
