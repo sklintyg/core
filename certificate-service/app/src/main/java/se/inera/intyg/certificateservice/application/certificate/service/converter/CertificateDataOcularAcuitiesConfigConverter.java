7ec724797b827955f364616a36064ceda7c8ee67
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-12-06 11:01:05 +0100

    K1J-558: Implemented CertificateDataOcularAcuitiesConfigConverter.

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateDataOcularAcuitiesConfigConverter.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateDataOcularAcuitiesConfigConverter.java
@@ -2,7 +2,10 @@ package se.inera.intyg.certificateservice.application.certificate.service.conver
 
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.application.certificate.dto.config.CertificateDataConfig;
+import se.inera.intyg.certificateservice.application.certificate.dto.config.CertificateDataConfigOcularAcuity;
+import se.inera.intyg.certificateservice.application.certificate.dto.config.OcularAcuity;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfigurationOcularAcuities;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementType;
 
@@ -17,6 +20,38 @@ public class CertificateDataOcularAcuitiesConfigConverter implements
 
   public CertificateDataConfig convert(ElementSpecification elementSpecification,
       Certificate certificate) {
-    throw new IllegalArgumentException();
+    if (!(elementSpecification.configuration() instanceof ElementConfigurationOcularAcuities configurationOcularAcuities)) {
+      throw new IllegalStateException(
+          "Invalid config type. Type was '%s'".formatted(
+              elementSpecification.configuration().type())
+      );
+    }
+
+    return CertificateDataConfigOcularAcuity.builder()
+        .id(configurationOcularAcuities.id().value())
+        .withCorrectionLabel(configurationOcularAcuities.withCorrectionLabel())
+        .withoutCorrectionLabel(configurationOcularAcuities.withoutCorrectionLabel())
+        .rightEye(
+            OcularAcuity.builder()
+                .withoutCorrectionId(configurationOcularAcuities.rightEye().withoutCorrectionId())
+                .withCorrectionId(configurationOcularAcuities.rightEye().withCorrectionId())
+                .label(configurationOcularAcuities.rightEye().label())
+                .build()
+        )
+        .leftEye(
+            OcularAcuity.builder()
+                .withoutCorrectionId(configurationOcularAcuities.leftEye().withoutCorrectionId())
+                .withCorrectionId(configurationOcularAcuities.leftEye().withCorrectionId())
+                .label(configurationOcularAcuities.leftEye().label())
+                .build()
+        )
+        .binocular(
+            OcularAcuity.builder()
+                .withoutCorrectionId(configurationOcularAcuities.binocular().withoutCorrectionId())
+                .withCorrectionId(configurationOcularAcuities.binocular().withCorrectionId())
+                .label(configurationOcularAcuities.binocular().label())
+                .build()
+        )
+        .build();
   }
 }
