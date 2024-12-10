6c4b880bfe78cbdc1ea2e1343fa7fb668165cdf7
Lucas Lundström <lucas.lundstroem@soprasteria.com>
2024-12-10 15:45:15 +0100

    K1J-590: add certificate-print-service module

---
+++ certificate-print-service/app/src/main/java/se/inera/intyg/certificateprintservice/infrastructure/AppConfig.java
@@ -0,0 +1,5 @@
+package se.inera.intyg.certificateprintservice.infrastructure;
+
+public class AppConfig {
+
+}
---
+++ certificate-print-service/domain/src/main/java/se/inera/intyg/certificateprintservice/domain/CertificatePdfGenerator.java
@@ -0,0 +1,5 @@
+package se.inera.intyg.certificateprintservice.domain;
+
+public class CertificatePdfGenerator {
+
+}
