d725c1ff04d8d11c5ef4240c17826429d80f46a8
Mikael Heglind <mikael.heglind@soprasteria.com>
2024-12-12 12:15:57 +0100

    K1J-592: Implemented PrintCertificateQuestionConverter

--- certificate-print-service/app/src/main/java/se/inera/intyg/certificateprintservice/application/print/converter/PrintCertificateQuestionConverter.java
+++ certificate-print-service/app/src/main/java/se/inera/intyg/certificateprintservice/application/print/converter/PrintCertificateQuestionConverter.java
@@ -2,13 +2,38 @@ package se.inera.intyg.certificateprintservice.application.print.converter;
 
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateprintservice.application.print.dto.PrintCertificateQuestionDTO;
+import se.inera.intyg.certificateprintservice.application.print.dto.value.ElementSimplifiedValueList;
+import se.inera.intyg.certificateprintservice.application.print.dto.value.ElementSimplifiedValueText;
 import se.inera.intyg.certificateprintservice.print.api.PrintCertificateQuestion;
+import se.inera.intyg.certificateprintservice.print.api.value.ElementValue;
+import se.inera.intyg.certificateprintservice.print.api.value.ElementValueList;
+import se.inera.intyg.certificateprintservice.print.api.value.ElementValueText;
 
 @Component
 public class PrintCertificateQuestionConverter {
 
   public PrintCertificateQuestion convert(PrintCertificateQuestionDTO question) {
     return PrintCertificateQuestion.builder()
+        .id(question.getId())
+        .name(question.getName())
+        .value(getElementValue(question))
+        .subQuestions(question.getSubQuestions().stream()
+            .map(this::convert)
+            .toList())
         .build();
+  }
+
+  private static ElementValue getElementValue(PrintCertificateQuestionDTO question) {
+    if (question.getValue() instanceof ElementSimplifiedValueText text) {
+      return ElementValueText.builder()
+          .text(text.getText())
+          .build();
+    } else if (question.getValue() instanceof ElementSimplifiedValueList list) {
+      return ElementValueList.builder()
+          .list(list.getList())
+          .build();
+    }
+
+    throw new IllegalArgumentException("Illegal value type");
   }
 }
