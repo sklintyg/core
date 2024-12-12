b4c751cf9f2798701401d6ba47f9ddc0b1cdba52
Lucas Lundström <lucas.lundstroem@soprasteria.com>
2024-12-12 10:45:05 +0100

    K1J-592: convert category

--- certificate-print-service/app/src/main/java/se/inera/intyg/certificateprintservice/application/print/converter/PrintCertificateCategoryConverter.java
+++ certificate-print-service/app/src/main/java/se/inera/intyg/certificateprintservice/application/print/converter/PrintCertificateCategoryConverter.java
@@ -7,8 +7,14 @@ import se.inera.intyg.certificateprintservice.print.api.PrintCertificateCategory
 @Component
 public class PrintCertificateCategoryConverter {
 
+  PrintCertificateQuestionConverter printCertificateQuestionConverter;
+
   public PrintCertificateCategory convert(PrintCertificateCategoryDTO category) {
     return PrintCertificateCategory.builder()
+        .id(category.getId())
+        .name(category.getName())
+        .children(category.getChildren().stream().map(printCertificateQuestionConverter::convert)
+            .toList())
         .build();
   }
 }
