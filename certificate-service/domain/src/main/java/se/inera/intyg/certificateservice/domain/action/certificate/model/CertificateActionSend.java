d5a2b3910df945ffcf8b51765fb343e337e86222
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-12-13 11:03:44 +0100

    K1J-607: CertificateActionSend should handle multiple recipients.

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/action/certificate/model/CertificateActionSend.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/action/certificate/model/CertificateActionSend.java
@@ -7,14 +7,22 @@ import lombok.Builder;
 import lombok.Getter;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateActionSpecification;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
+import se.inera.intyg.certificateservice.domain.common.model.Recipient;
 
 @Builder
 @Getter(AccessLevel.NONE)
 public class CertificateActionSend implements CertificateAction {
 
-  private static final String NAME = "Skicka till Försäkringskassan";
-  private static final String DESCRIPTION = "Öppnar ett fönster där du kan välja att skicka intyget till Försäkringskassan.";
-  private static final String BODY = "<p>Om du går vidare kommer intyget skickas direkt till Försäkringskassans system vilket ska göras i samråd med patienten.</p>";
+  private static final String NAME_FK = "Skicka till Försäkringskassan";
+  private static final String DESCRIPTION_FK = "Öppnar ett fönster där du kan välja att skicka intyget till Försäkringskassan.";
+  private static final String BODY_FK = "<p>Om du går vidare kommer intyget skickas direkt till Försäkringskassans system vilket ska göras i samråd med patienten.</p>";
+  private static final String NAME_TS = "Skicka till Transportstyrelsen";
+  private static final String DESCRIPTION_TS = "Öppnar ett fönster där du kan välja att skicka intyget till Transportstyrelsen.";
+  private static final String BODY_TS = "<p>Om du går vidare kommer intyget skickas direkt till Transportstyrelsens system vilket ska göras i samråd med patienten.</p>";
+  private static final String TRANSPORTSTYRELSEN = "Transportstyrelsen";
+  private static final String FORSAKRINGSKASSAN = "Försäkringskassan";
+  private static final String UNSUPPORTED_RECIPIENT = "Unsupported recipient: %s";
   private final CertificateActionSpecification certificateActionSpecification;
   private final List<ActionRule> actionRules;
 
@@ -42,17 +50,56 @@ public class CertificateActionSend implements CertificateAction {
 
   @Override
   public String getName(Optional<Certificate> certificate) {
-    return NAME;
+    return certificate
+        .map(Certificate::certificateModel)
+        .map(CertificateModel::recipient)
+        .map(CertificateActionSend::recipientName)
+        .orElseThrow(
+            () -> new IllegalStateException("Unable to retrieve recipient for certificate"));
   }
 
   @Override
   public String getDescription(Optional<Certificate> certificate) {
-    return DESCRIPTION;
+    return certificate
+        .map(Certificate::certificateModel)
+        .map(CertificateModel::recipient)
+        .map(CertificateActionSend::recipientDescription)
+        .orElseThrow(
+            () -> new IllegalStateException("Unable to retrieve recipient for certificate"));
   }
 
   @Override
   public String getBody(Optional<Certificate> certificate,
       Optional<ActionEvaluation> actionEvaluation) {
-    return BODY;
+    return certificate
+        .map(Certificate::certificateModel)
+        .map(CertificateModel::recipient)
+        .map(CertificateActionSend::recipientBody)
+        .orElseThrow(
+            () -> new IllegalStateException("Unable to retrieve recipient for certificate"));
+  }
+
+  private static String recipientName(Recipient recipient) {
+    return switch (recipient.name()) {
+      case TRANSPORTSTYRELSEN -> NAME_TS;
+      case FORSAKRINGSKASSAN -> NAME_FK;
+      default -> throw new IllegalStateException(UNSUPPORTED_RECIPIENT.formatted(recipient.name()));
+    };
+  }
+
+  private static String recipientBody(Recipient recipient) {
+    return switch (recipient.name()) {
+      case TRANSPORTSTYRELSEN -> BODY_TS;
+      case FORSAKRINGSKASSAN -> BODY_FK;
+      default -> throw new IllegalStateException(UNSUPPORTED_RECIPIENT.formatted(recipient.name()));
+    };
+  }
+
+  private static String recipientDescription(Recipient recipient) {
+    return switch (recipient.name()) {
+      case TRANSPORTSTYRELSEN -> DESCRIPTION_TS;
+      case FORSAKRINGSKASSAN -> DESCRIPTION_FK;
+      default -> throw new IllegalStateException(UNSUPPORTED_RECIPIENT.formatted(recipient.name()));
+    };
   }
 }
