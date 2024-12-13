857bf043e80c9577e57e78ec809e0efebad52e29
Lucas Lundström <lucas.lundstroem@soprasteria.com>
2024-12-13 14:31:08 +0100

    K1J-592: add page headers based on metadata

--- certificate-print-service/playwright-pdf-generator/src/main/java/se/inera/intyg/certificateprintservice/print/CertificatePrintGenerator.java
+++ certificate-print-service/playwright-pdf-generator/src/main/java/se/inera/intyg/certificateprintservice/print/CertificatePrintGenerator.java
@@ -6,6 +6,7 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Page.PdfOptions;
 import com.microsoft.playwright.Playwright;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.text.html.HTML.Tag;
@@ -20,6 +21,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.certificateprintservice.print.api.Category;
 import se.inera.intyg.certificateprintservice.print.api.Certificate;
+import se.inera.intyg.certificateprintservice.print.api.Metadata;
 import se.inera.intyg.certificateprintservice.print.api.Question;
 import se.inera.intyg.certificateprintservice.print.api.value.ElementValueList;
 import se.inera.intyg.certificateprintservice.print.api.value.ElementValueText;
@@ -29,14 +31,16 @@ import se.inera.intyg.certificateprintservice.print.api.value.ElementValueText;
 @RequiredArgsConstructor
 public class CertificatePrintGenerator implements PrintCertificateGenerator {
 
-  private final GeneratorService generatorService;
+//  private final GeneratorService generatorService;
 
   @Value("classpath:templates/certificateTemplate.html")
   private Resource template;
+  @Value("classpath:transportstyrelsens_logotyp_rgb.eps")
+  private Resource logotype;
 
   @Override
-  public byte[] generate(Certificate request) {
-    String jsoupContent = convertToHtml(request);
+  public byte[] generate(Certificate certificate) {
+    String jsoupContent = convertToHtml(certificate);
 
     try (
         Playwright playwright = Playwright.create();
@@ -45,21 +49,45 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
         BrowserContext context = browser.newContext();
         Page page = context.newPage()
     ) {
-      final var pdfOptions = createtPdfOptions();
+      final var pdfOptions = createtPdfOptions(certificate.getMetadata());
 
       page.setContent(jsoupContent);
+
       return page.pdf(pdfOptions);
+    } catch (IOException e) {
+      throw new RuntimeException("Could not get logo", e);
     }
   }
 
-  private PdfOptions createtPdfOptions() {
+
+  private String convertToHtml(Certificate certificate) {
+    try {
+      var doc = Jsoup.parse(template.getFile(), "UTF-8", "", Parser.xmlParser());
+      Element title = doc.getElementById("title");
+      title.appendText(
+          "%s (%s %s)".formatted(certificate.getMetadata().getName(),
+              certificate.getMetadata().getTypeId(),
+              certificate.getMetadata().getVersion()));
+      Element content = doc.getElementById("content");
+      content.attr("style", "margin-top: 200px;");
+      certificate.getCategories().forEach(category -> content.appendChild(
+          convertCategory(category, certificate.getCategories().indexOf(category))));
+
+      log.info(doc.html());
+      return doc.html();
+    } catch (Exception e) {
+      throw new RuntimeException("Could not build html from certificate config", e);
+    }
+  }
+
+  private PdfOptions createtPdfOptions(Metadata certificateMetadata) throws IOException {
     var pdfOptions = new PdfOptions();
     pdfOptions.setFormat("A4");
     pdfOptions.setPrintBackground(true);
     pdfOptions.setDisplayHeaderFooter(true);
     pdfOptions.setTagged(true);
 
-    pdfOptions.setHeaderTemplate("<title>Läkarintyg för högre körkortsbehörighet</title>");
+    pdfOptions.setHeaderTemplate(createHeader(certificateMetadata));
     pdfOptions.setFooterTemplate("""
         <footer>
           <p>Utskriften skapades med Webcert - en tjänst som drivs av Inera AB</p>
@@ -67,20 +95,24 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
     return pdfOptions;
   }
 
-  private String convertToHtml(Certificate request) {
-    try {
-      var doc = Jsoup.parse(template.getFile(), "UTF-8", "", Parser.xmlParser());
-      Element title = doc.getElementById("title");
-      title.appendText(request.getMetadata().getName());
-      Element content = doc.getElementById("content");
-      request.getCategories().forEach(category -> content.appendChild(
-          convertCategory(category, request.getCategories().indexOf(category))));
+  private String createHeader(Metadata certificateMetadata) throws IOException {
+    final var headerDiv = new Element(Tag.DIV.toString());
+    headerDiv.appendChild(new Element(Tag.P.toString()).attr("class", "title").attr("style",
+        "font-size: 14px; width: 800px; height: 200px; margin: 20px;"));
+    headerDiv.appendChild(new Element(Tag.P.toString()).attr("class", "pageNumber").attr("style",
+        "font-size: 10px; width: 15px; height: 200px; margin-top: 20px;"));
+    headerDiv.appendChild(new Element(Tag.P.toString()).attr("class", "totalPages").attr("style",
+        "font-size: 10px; width: 15px; height: 200px; margin-top: 20px;"));
 
-      log.info(doc.html());
-      return doc.html();
-    } catch (Exception e) {
-      throw new RuntimeException("Could not build html from certificate config", e);
-    }
+    final var pnrDiv = new Element(Tag.DIV.toString());
+    pnrDiv.appendChild(new Element(Tag.STRONG.toString()).appendText("Person- /samordningsnr"));
+    pnrDiv.appendChild(new Element(Tag.P.toString()).appendText(certificateMetadata.getPersonId()));
+    headerDiv.appendChild(pnrDiv);
+
+//    headerDiv.appendChild(
+//        new Element(Tag.IMG.toString()).attr("src", logotype.getURI().toString()));
+
+    return headerDiv.html();
   }
 
   private Node convertCategory(Category category, int index) {
