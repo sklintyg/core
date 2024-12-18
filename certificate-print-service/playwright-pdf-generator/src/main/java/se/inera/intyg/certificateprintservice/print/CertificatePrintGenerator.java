4c086c6f52543effe5d1b729e6cf629aed0498f5
Lucas Lundström <lucas.lundstroem@soprasteria.com>
2024-12-18 10:36:48 +0100

    K1J-592: add left page info, watermark ukast

--- certificate-print-service/playwright-pdf-generator/src/main/java/se/inera/intyg/certificateprintservice/print/CertificatePrintGenerator.java
+++ certificate-print-service/playwright-pdf-generator/src/main/java/se/inera/intyg/certificateprintservice/print/CertificatePrintGenerator.java
@@ -37,9 +37,10 @@ import se.inera.intyg.certificateprintservice.print.api.value.ElementValueText;
 public class CertificatePrintGenerator implements PrintCertificateGenerator {
 
   private static final String STYLE = "style";
-  private static final String CLASS = "class";
   @Value("classpath:templates/certificateTemplate.html")
   private Resource template;
+  @Value("classpath:templates/tailwind_3.4.16.js")
+  private Resource cssScript;
   private final Browser browser;
 
 
@@ -81,24 +82,20 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
       certificate.getCategories().forEach(category -> content.appendChild(
           convertCategory(category, certificate.getCategories().indexOf(category))));
 
-//      final var logoPath = ClassLoader.getSystemResource("transportstyrelsen-logo.png")
-//          .toURI();
-//      byte[] imageBytes = Files.readAllBytes(Paths.get(logoPath));
-//      final var base64 = Base64.getEncoder().encode(imageBytes);
-//      final var img = new Element(Tag.IMG.toString())
-//          .attr("src", "data:image/png;base64, " + new String(base64))
-//          .attr("alt", "test-image-alt")
-//          .attr("style", "height: 40px");
-//
-//      final var logo = doc.getElementById("logo").appendChild(img);
-//      final var pnr = doc.getElementById("pnr").attr("style", "font-weight: bold;")
-//          .appendText("Person- /samordningsnr");
+      doc.getElementById("style").appendText("""
+          @page {
+                margin: %dmm 20mm 15mm 20mm;
+              }""".formatted(calculateTopMargin()));
 
       log.info(doc.html());
       return doc.html();
     } catch (Exception e) {
       throw new RuntimeException("Could not build html from certificate config", e);
     }
+  }
+
+  private int calculateTopMargin() {
+    return 30;
   }
 
   private PdfOptions createtPdfOptions(Metadata certificateMetadata)
@@ -109,23 +106,39 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
     pdfOptions.setDisplayHeaderFooter(true);
     pdfOptions.setTagged(true);
     pdfOptions.setHeaderTemplate(createHeader(certificateMetadata));
-//    pdfOptions.setFooterTemplate("""
-//        <footer>
-//          <p>Utskriften skapades med Webcert - en tjänst som drivs av Inera AB</p>
-//        </footer>""");
+    pdfOptions.setFooterTemplate(createFooterTemplate(certificateMetadata));
     pdfOptions.setPath(Path.of("certificate.pdf"));
 
     return pdfOptions;
   }
 
+  private String createFooterTemplate(Metadata certificateMetadata) {
+
+    final var footerDiv = new Element(Tag.DIV.toString());
+    footerDiv.attr("style", "width: 100%; font-size: 10px;");
+    final var pageNrSpan = new Element(Tag.SPAN.toString()).addClass("pageNumber");
+    final var totalPageNrSpan = new Element(Tag.SPAN.toString()).addClass("totalPageNumber");
+
+    footerDiv.appendText("Sida ");
+    footerDiv.appendChild(pageNrSpan);
+    footerDiv.appendText("(");
+    footerDiv.appendChild(totalPageNrSpan);
+    footerDiv.appendText(")").html();
+//    return footerDiv.html();
+    return """
+            <div style="width: 100%; text-align: right; font-size: 10px; margin-right: 30mm">
+                Sida <span class="pageNumber"></span>
+                (<span class="totalPages"></span>)
+            </div>
+        """;
+  }
+
   private String createHeader(Metadata certificateMetadata) throws IOException, URISyntaxException {
 
     final var headerDiv = new Element(Tag.DIV.toString()).attr("style",
-        "display: flex; font-size: 10px; width: 100%; justifyContent: flex-end; padding: 0 20px; gap: 10px;");
+        "display: flex; font-size: 10px; height: 100%; justify-content: center; align-items: center;");
     headerDiv.appendChild(
         new Element(Tag.P.toString()).addClass("title").attr("style", "font-size: 10px;"));
-
-    final var pageNrDiv = new Element(Tag.DIV.toString());
 
     final var pnrDiv = new Element(Tag.DIV.toString());
     pnrDiv.appendChild(new Element(Tag.STRONG.toString()).appendText("Person- /samordningsnr"))
@@ -133,12 +146,6 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
     pnrDiv.appendChild(new Element(Tag.P.toString()).appendText(certificateMetadata.getPersonId()))
         .attr("style", "font-size: 10px;");
     headerDiv.appendChild(pnrDiv);
-
-    pageNrDiv.appendChild(
-        new Element(Tag.P.toString()).addClass("pageNumber").attr("style", "font-size: 10px;"));
-    pageNrDiv.appendChild(
-        new Element(Tag.P.toString()).addClass("totalPages").attr("style", "font-size: 10px;"));
-    headerDiv.appendChild(pageNrDiv);
 
     final var logoPath = ClassLoader.getSystemResource("transportstyrelsen-logo.png")
         .toURI();
@@ -151,17 +158,46 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
 
     headerDiv.appendChild(img);
 
-    final var leftPageInfo = new Element(Tag.DIV.toString()).addClass(
-        "fixed top-0 bottom 0 left-0 leading-4 text-sm");
-    leftPageInfo.appendChild(
-        new Element(Tag.DIV.toString()).addClass("rotate-180")
-            .attr("style",
-                "font-size: 10px; writingMode: vertical-rl; textOrientation: sideways; height: 100%;")
-            .appendChild(new Element(Tag.P.toString()).text("HEJSANHOPPSANHEJSAN"))
-
-    );
-
+    final var leftPageInfo = new Element(Tag.DIV.toString()).attr("style",
+        """
+            display: flex;
+            justify-content: start;
+            align-items: end;
+            position: fixed;
+            border: red solid 1px;
+            transform: translateX(-50%);
+            font-size; 10px;
+            writingMode: vertical-rl;
+            textOrientation: sideways;
+            height: 100%;
+            bottom: 40mm;
+            width: 20mm;
+            text-wrap: nowrap;
+            """);
+    final var child = new Element(Tag.SPAN.toString());
+    child.attr("style",
+        "font-size: 10px; transform: rotate(-90deg);");
+    child.text("%s utfärdat av %s".formatted(certificateMetadata.getTypeId(),
+        certificateMetadata.getApplicationOrigin()));
+    leftPageInfo.appendChild(child);
     headerDiv.appendChild(leftPageInfo);
+
+    final var watermark = new Element(Tag.DIV.toString()).attr("style", """
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        height: 100%;
+        transform: translate(-50%, -50%) rotate(45deg);
+        font-family: 'Helvetica', sans-serif;
+        font-size: 100pt;
+        color: rgb(128, 128, 128);
+        opacity: 0.5;
+        z-index: -1;
+        """
+    );
+    watermark.text("UTKAST");
+    headerDiv.appendChild(watermark);
 
     return headerDiv.html();
   }
@@ -169,8 +205,9 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
   private Node convertCategory(Category category, int index) {
     final var div = new Element(Tag.DIV.toString());
     div.attr(STYLE, "border: 1px solid black; margin-top 2cm;");
+    div.addClass("box-decoration-clone");
     final var title = new Element(Tag.H2.toString());
-    title.attr(CLASS, "text-lg font-bold");
+    title.addClass("text-lg font-bold");
     title.attr(STYLE, "border-bottom: 1px solid black;");
     title.text("%d %s".formatted(index + 1, category.getName()));
     div.appendChild(title);
@@ -181,11 +218,11 @@ public class CertificatePrintGenerator implements PrintCertificateGenerator {
   private List<Node> convertQuestion(Question question) {
     final var list = new ArrayList<Node>();
     final var name = new Element(Tag.H3.toString());
-    name.attr(CLASS, "p-1");
+    name.addClass("p-1");
     name.text("%s".formatted(question.getName()));
     list.add(name);
     final var value = new Element(Tag.P.toString());
-    value.attr(CLASS, "text-sm p-1");
+    value.addClass("text-sm p-1");
     if (question.getValue() instanceof ElementValueText textValue) {
       value.appendText(textValue.getText());
     } else if (question.getValue() instanceof ElementValueList listValue) {
