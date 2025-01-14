735cbca6a651f316d7cd5e71e7c3dc786fd6fa8b
Mikael Heglind <mikael.heglind@soprasteria.com>
2025-01-14 10:43:05 +0100

    LOCALENV: set browsers dir

--- certificate-print-service/playwright-pdf-generator/src/main/java/se/inera/intyg/certificateprintservice/playwright/browserpool/BrowserFactory.java
+++ certificate-print-service/playwright-pdf-generator/src/main/java/se/inera/intyg/certificateprintservice/playwright/browserpool/BrowserFactory.java
@@ -2,6 +2,9 @@ package se.inera.intyg.certificateprintservice.playwright.browserpool;
 
 import com.microsoft.playwright.BrowserType.LaunchOptions;
 import com.microsoft.playwright.Playwright;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.impl.DefaultPooledObject;
@@ -10,8 +13,13 @@ public class BrowserFactory extends BasePooledObjectFactory<PlaywrightBrowser> {
 
   @Override
   public PlaywrightBrowser create() {
-    final var playwright = Playwright.create();
-    final var launchOptions = new LaunchOptions().setHeadless(true);
+    Map<String, String> env = new HashMap<>();
+    //env.put("PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD", "1");
+    env.put("PLAYWRIGHT_BROWSERS_PATH",
+        Paths.get("/browsers").toString());
+    final var playwright = Playwright.create(new Playwright.CreateOptions().setEnv(env));
+    final var launchOptions = new LaunchOptions()
+        .setHeadless(true);
     final var browser = playwright.chromium().launch(launchOptions);
     return new PlaywrightBrowser(playwright, browser);
   }
