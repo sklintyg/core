e7cd21fbb784f7984ac63609ea6bfc2ba572d16b
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2025-03-18 10:28:53 +0100

    K1J-871: Updated dev enviroments to run CTS locally. Updated fields and removed page from CertificateExportPageDTO (#415)

--- customer-terminate-service/app/src/main/java/se/inera/intyg/cts/infrastructure/integration/Intygstjanst/dto/CertificateExportPageDTO.java
+++ customer-terminate-service/app/src/main/java/se/inera/intyg/cts/infrastructure/integration/Intygstjanst/dto/CertificateExportPageDTO.java
@@ -2,7 +2,7 @@ package se.inera.intyg.cts.infrastructure.integration.Intygstjanst.dto;
 
 import java.util.List;
 
-public record CertificateExportPageDTO(String careProviderId, int page, int count, long total,
+public record CertificateExportPageDTO(String careProviderId, int count, long total,
                                        long totalRevoked, List<CertificateXmlDTO> certificateXmls) {
 
 }
