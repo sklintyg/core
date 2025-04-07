1b9fef99045091278e7613b8ca883416a8bbce11
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-04-07 13:05:27 +0200

    HACK: System prompt can be configured

--- certificate-service/ai-prefill/src/main/java/se/inera/intyg/certificateservice/prefill/service/PrefillService.java
+++ certificate-service/ai-prefill/src/main/java/se/inera/intyg/certificateservice/prefill/service/PrefillService.java
@@ -6,6 +6,7 @@ import model.AIPrefillValueDate;
 import model.AIPrefillValueDateList;
 import model.AIPrefillValueDiagnosisList;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
@@ -29,6 +30,9 @@ public class PrefillService {
   private static final String SYSTEM_PROMPT = "Generate a map placing the question ids as the key. The value you need to extract from the prompt. In the prompt you will recieve a certificatemodel which has different questions defined with an id and a question name. Using the question name you will extract data from the text, which is electronic health records for the patient, and set as the value in the map. The question id is the key.";
   private static final String ONLY_TEXT_PROMPT = "The value will be strings so if you in the model find something that is not a string, skip that value and dont add it to the map.";
   private static final String MORE_VALUES_PROMPT = "The value will be strings, date, date list (you will use the id and the date to create a list), checkboxes (codes that you will take from the config), and diagnosis so if you in the model find something that is not these values, skip that value and dont add it to the map. When you've generated the whole map, then add a entry in the map with key INFO where you add a text that describes what information you have used and how you have used it.";
+
+  @Value("${ai.prefill.system.prompt}")
+  private String systemPrompt;
 
   private final ChatClient chatClient;
 
@@ -57,7 +61,7 @@ public class PrefillService {
 
   private Map<String, AIPrefillValue> generateMap(Certificate certificate, String ehrData) {
     return chatClient.prompt()
-        .system(SYSTEM_PROMPT + MORE_VALUES_PROMPT)
+        .system(systemPrompt != null && !systemPrompt.isEmpty() ? systemPrompt : SYSTEM_PROMPT + MORE_VALUES_PROMPT)
         .user(String.format("CertificateModel: %s, \n Text: %s", certificate.certificateModel(),
             ehrData))
         .call()
