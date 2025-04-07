f981980b48daa31f809dc4dc91a978345692b5b0
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2025-04-07 11:20:13 +0200

    HACK: Implementation of app service for AI-prefill

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/CertificateAIPrefillService.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/CertificateAIPrefillService.java
@@ -1,18 +1,57 @@
 package se.inera.intyg.certificateservice.application.certificate.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateAIPrefillRequest;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateAIPrefillResponse;
+import se.inera.intyg.certificateservice.application.certificate.service.converter.CertificateConverter;
+import se.inera.intyg.certificateservice.application.common.ActionEvaluationFactory;
+import se.inera.intyg.certificateservice.application.common.converter.ResourceLinkConverter;
+import se.inera.intyg.certificateservice.domain.certificate.model.CertificateId;
+import se.inera.intyg.certificateservice.domain.certificate.repository.CertificateRepository;
+import se.inera.intyg.certificateservice.prefill.PrefillService;
 
 @Service
 @RequiredArgsConstructor
 public class CertificateAIPrefillService {
 
+  private final CertificateRepository certificateRepository;
+  private final PrefillService prefillService;
+  private final CertificateConverter certificateConverter;
+  private final ActionEvaluationFactory actionEvaluationFactory;
+  private final ResourceLinkConverter resourceLinkConverter;
 
   public CertificateAIPrefillResponse prefill(CertificateAIPrefillRequest request,
       String certificateId) {
 
-    return null;
+    final var actionEvaluation = actionEvaluationFactory.create(
+        request.getUser(),
+        request.getUnit(),
+        request.getCareUnit(),
+        request.getCareProvider()
+    );
+
+    final var certificate = certificateRepository.getById(new CertificateId(certificateId));
+    final var prefilledCertificate = prefillService.prefill(certificate, request.getPreFillData());
+    certificateRepository.save(prefilledCertificate);
+
+    return CertificateAIPrefillResponse.builder()
+        .certificate(
+            certificateConverter.convert(
+                certificate,
+                certificate.actionsInclude(Optional.of(actionEvaluation)).stream()
+                    .map(certificateAction ->
+                        resourceLinkConverter.convert(
+                            certificateAction,
+                            Optional.of(certificate),
+                            actionEvaluation
+                        )
+                    )
+                    .toList(),
+                actionEvaluation
+            )
+        )
+        .build();
   }
 }
