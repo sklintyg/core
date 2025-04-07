3b69618d7acbdf7562ce1f89418b51d7c636333b
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-04-07 13:09:57 +0200

    HACK: Fix

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/CertificateAIPrefillService.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/CertificateAIPrefillService.java
@@ -33,8 +33,8 @@ public class CertificateAIPrefillService {
     );
 
     final var certificate = certificateRepository.getById(new CertificateId(certificateId));
-    final var prefilledCertificate = prefillService.prefill(certificate, request.getPreFillData());
-    certificateRepository.save(prefilledCertificate);
+    final var prefilledResponse = prefillService.prefill(certificate, request.getPreFillData());
+    certificateRepository.save(prefilledResponse.getCertificate());
 
     return CertificateAIPrefillResponse.builder()
         .certificate(
