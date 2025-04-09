39640c1335aee6f5299ae5e1f21fa419eb0a84fb
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-04-09 10:16:55 +0200

    K1J-966: Added CertificateModelFactoryFK7427.

---
+++ certificate-service/app/src/test/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7427/CertificateModelFactoryFK7427Test.java
@@ -0,0 +1,136 @@
+package se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7427;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.test.util.ReflectionTestUtils;
+import se.inera.intyg.certificateservice.domain.action.certificate.model.CertificateActionFactory;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModelId;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateType;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateVersion;
+import se.inera.intyg.certificateservice.domain.common.model.Recipient;
+import se.inera.intyg.certificateservice.domain.common.model.RecipientId;
+
+class CertificateModelFactoryFK7427Test {
+
+  @Mock
+  private CertificateActionFactory certificateActionFactory;
+  private static final String TYPE = "fk7427";
+  private static final String VERSION = "1.0";
+  private CertificateModelFactoryFK7427 certificateModelFactoryFK7427;
+
+  private static final String LOGICAL_ADDRESS = "L-A";
+
+  @BeforeEach
+  void setUp() {
+    certificateModelFactoryFK7427 = new CertificateModelFactoryFK7427(certificateActionFactory);
+
+    ReflectionTestUtils.setField(certificateModelFactoryFK7427, "fkLogicalAddress",
+        LOGICAL_ADDRESS);
+  }
+
+  @Test
+  void shallIncludeId() {
+    final var expectedId =
+        CertificateModelId.builder()
+            .type(new CertificateType(TYPE))
+            .version(new CertificateVersion(VERSION))
+            .build();
+
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertEquals(expectedId, certificateModel.id());
+  }
+
+  @Test
+  void shallIncludeName() {
+    final var expectedName = "Läkarutlåtande tillfällig föräldrapenning barn 12–16 år";
+
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertEquals(expectedName, certificateModel.name());
+  }
+
+  @Test
+  void shallIncludeDescription() {
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertFalse(certificateModel.description().isBlank());
+  }
+
+  @Test
+  void shallIncludeDetailedDescription() {
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertFalse(certificateModel.detailedDescription().isBlank());
+  }
+
+  @Test
+  void shallIncludeActiveFrom() {
+    final var expectedActiveFrom = LocalDateTime.now(ZoneId.systemDefault());
+    ReflectionTestUtils.setField(
+        certificateModelFactoryFK7427,
+        "activeFrom",
+        expectedActiveFrom
+    );
+
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertEquals(expectedActiveFrom, certificateModel.activeFrom());
+  }
+
+  @Test
+  void shallIncludeAvailableForCitizen() {
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertFalse(certificateModel.availableForCitizen());
+  }
+
+  @Test
+  void shallIncludeRecipient() {
+    final var expectedRecipient = new Recipient(
+        new RecipientId("FKASSA"),
+        "Försäkringskassan",
+        LOGICAL_ADDRESS
+    );
+
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertEquals(expectedRecipient, certificateModel.recipient());
+  }
+
+  @Test
+  void shallIncludeCertificateActionSpecifications() {
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertAll(
+        () -> assertNotNull(certificateModel.certificateActionSpecifications()),
+        () -> assertFalse(certificateModel.certificateActionSpecifications().isEmpty())
+    );
+  }
+
+  @Test
+  void shallIncludeMessageActionSpecifications() {
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertAll(
+        () -> assertNotNull(certificateModel.messageActionSpecifications()),
+        () -> assertFalse(certificateModel.messageActionSpecifications().isEmpty())
+    );
+  }
+
+
+  @Test
+  void shallIncludeCertificateActionFactory() {
+    final var certificateModel = certificateModelFactoryFK7427.create();
+
+    assertEquals(certificateActionFactory, certificateModel.certificateActionFactory());
+  }
+}
