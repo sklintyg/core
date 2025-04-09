2ba6e5a2d4cb69b7323214d62f1a15e0c24338cc
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-04-09 12:03:49 +0200

    K1J-966: Updated FK7427CertificateActionSpecification and added tests for FK7427MessageActionSpecification.

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7427/FK7427CertificateActionSpecification.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7427/FK7427CertificateActionSpecification.java
@@ -12,73 +12,54 @@ public class FK7427CertificateActionSpecification {
   }
 
   public static List<CertificateActionSpecification> create() {
-    final var allowedRolesForProtectedPersons = List.of(
-        Role.DOCTOR,
-        Role.PRIVATE_DOCTOR,
-        Role.NURSE,
-        Role.MIDWIFE
-    );
     return List.of(
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.CREATE)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.READ)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.UPDATE)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.DELETE)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.SIGN)
             .allowedRoles(
-                List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.NURSE, Role.MIDWIFE)
+                List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR)
             )
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.SEND)
             .allowedRoles(
-                List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.NURSE, Role.MIDWIFE, Role.CARE_ADMIN)
+                List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.CARE_ADMIN)
             )
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.PRINT)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.REVOKE)
             .allowedRoles(
-                List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.NURSE, Role.MIDWIFE)
+                List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR)
             )
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.REPLACE)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.REPLACE_CONTINUE)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.RENEW)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.SEND_AFTER_SIGN)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.MESSAGES)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.MESSAGES_ADMINISTRATIVE)
@@ -91,22 +72,18 @@ public class FK7427CertificateActionSpecification {
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.COMPLEMENT)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.SEND_AFTER_COMPLEMENT)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.CANNOT_COMPLEMENT)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.FORWARD_MESSAGE)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.HANDLE_COMPLEMENT)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.FORWARD_CERTIFICATE)
@@ -118,16 +95,13 @@ public class FK7427CertificateActionSpecification {
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.LIST_CERTIFICATE_TYPE)
-            .allowedRoles(List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.NURSE, Role.MIDWIFE,
-                Role.CARE_ADMIN))
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
+            .allowedRoles(List.of(Role.DOCTOR, Role.PRIVATE_DOCTOR, Role.CARE_ADMIN))
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.QUESTIONS_NOT_AVAILABLE)
             .build(),
         CertificateActionSpecification.builder()
             .certificateActionType(CertificateActionType.FORWARD_CERTIFICATE_FROM_LIST)
-            .allowedRolesForProtectedPersons(allowedRolesForProtectedPersons)
             .build()
     );
   }
