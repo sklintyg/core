d18c3c52b48482fa8ec3570a26cca3734bce20b7
Mikael Heglind <mikael.heglind@soprasteria.com>
2025-04-12 15:30:06 +0200

    K1J-967: Added test to assert thread safety of trim method

--- certificate-service/clinicalprocess-certificate-v4/src/test/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/common/XmlNamespaceTrimmerTest.java
+++ certificate-service/clinicalprocess-certificate-v4/src/test/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/common/XmlNamespaceTrimmerTest.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.junit.jupiter.api.Test;
 
@@ -61,5 +65,36 @@ class XmlNamespaceTrimmerTest {
     final var invalidXml = "<invalid>";
     final var result = XmlNamespaceTrimmer.trim(invalidXml);
     assertEquals(invalidXml, result);
+  }
+
+  @Test
+  void shouldFailIfNotThreadSafe() throws InterruptedException {
+    final int threadCount = 10;
+    final int iterations = 10;
+    final var latch = new CountDownLatch(threadCount);
+    final var errors = new ConcurrentLinkedQueue<String>();
+    final var excpectedXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ns4:Root xmlns:ns4=\"https://example.com/root\"><ns4:Child xmlns:ns6=\"https://example.com/usedChild\"><ns4:Element>Value</ns4:Element><ns6:SubElement>SubValue</ns6:SubElement></ns4:Child></ns4:Root>";
+
+    try (final var executorService = Executors.newFixedThreadPool(threadCount)) {
+      for (int i = 0; i < threadCount; i++) {
+        executorService.submit(() -> {
+          try {
+            for (int j = 0; j < iterations; j++) {
+              final var result = XmlNamespaceTrimmer.trim(INPUT_XML);
+              if (!result.equals(excpectedXml)) {
+                errors.add("Inconsistent result detected");
+              }
+            }
+          } finally {
+            latch.countDown();
+          }
+        });
+      }
+      latch.await();
+    }
+
+    if (!errors.isEmpty()) {
+      fail("Thread safety test failed: " + errors);
+    }
   }
 }
