f2c71ac993a163315dda5b36031187a153c438c6
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-04-23 13:27:00 +0200

    K1J-1009: Added integration tests

---
+++ certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/fk7426/FK7426Constants.java
@@ -0,0 +1,12 @@
+package se.inera.intyg.certificateservice.integrationtest.fk7426;
+
+public class FK7426Constants {
+
+  public static final String FK7426 = "fk7426";
+  public static final String FK7426_DISPLAY_TYPE = "FK7426";
+  public static final String VERSION = "1.0";
+  public static final String WRONG_VERSION = "wrongVersion";
+  public static final String TYPE = FK7426_DISPLAY_TYPE + " - ";
+  public static final String CODE = "LU_TFP_ASB18";
+  public static final String CODE_SYSTEM = "b64ea353-e8f6-4832-b563-fc7d46f29548";
+}
