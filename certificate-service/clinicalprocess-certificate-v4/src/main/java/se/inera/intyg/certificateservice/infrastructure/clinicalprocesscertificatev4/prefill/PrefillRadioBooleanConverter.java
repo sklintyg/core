24646ba2b1068f5bd1f3c7413a7665f2cd7c666e
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-06-24 10:14:40 +0200

    K1J-1171: Updated converter for PrefillRadioBooleanConverter

--- certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/prefill/PrefillRadioBooleanConverter.java
+++ certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/prefill/PrefillRadioBooleanConverter.java
@@ -1,16 +1,16 @@
 package se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertificatev4.prefill;
 
-import java.util.Collections;
 import java.util.List;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementData;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueBoolean;
-import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfiguration;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfigurationRadioBoolean;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
+import se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertificatev4.prefill.util.PrefillValidator;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Svar;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Svar.Delsvar;
+import se.riv.clinicalprocess.healthcond.certificate.v33.Forifyllnad;
 
 @Component
 public class PrefillRadioBooleanConverter implements PrefillConverter {
@@ -21,59 +21,68 @@ public class PrefillRadioBooleanConverter implements PrefillConverter {
   }
 
   @Override
-  public PrefillAnswer prefillSubAnswer(List<Delsvar> subAnswers,
-      ElementSpecification specification) {
+  public PrefillAnswer prefillAnswer(ElementSpecification specification,
+      Forifyllnad prefill) {
     if (!(specification.configuration() instanceof ElementConfigurationRadioBoolean configurationRadioBoolean)) {
       return PrefillAnswer.builder()
           .errors(List.of(PrefillError.wrongConfigurationType()))
           .build();
     }
 
-    if (subAnswers.size() != 1) {
+    final var answers = prefill.getSvar().stream()
+        .filter(svar -> svar.getId().equals(specification.id().id()))
+        .toList();
+
+    final var subAnswers = prefill.getSvar().stream()
+        .map(Svar::getDelsvar)
+        .flatMap(List::stream)
+        .filter(delsvar -> delsvar.getId().equals(specification.id().id()))
+        .toList();
+
+    final var prefillError = PrefillValidator.validateSingleAnswerOrSubAnswer(
+        answers,
+        subAnswers,
+        specification
+    );
+
+    if (prefillError != null) {
       return PrefillAnswer.builder()
-          .errors(List.of(
-              PrefillError.wrongNumberOfSubAnswers(specification.id().id(), 1, subAnswers.size())))
+          .errors(List.of(prefillError))
           .build();
     }
 
-    boolean value;
     try {
-      value = Boolean.parseBoolean((String) subAnswers.getFirst().getContent().getFirst());
+      final var noPrefill = subAnswers.isEmpty() && answers.isEmpty();
+      return noPrefill
+          ? null
+          : PrefillAnswer.builder()
+              .elementData(
+                  ElementData.builder()
+                      .id(specification.id())
+                      .value(
+                          ElementValueBoolean.builder()
+                              .booleanId(configurationRadioBoolean.id())
+                              .value(Boolean.parseBoolean(getContent(subAnswers, answers)))
+                              .build()
+                      )
+                      .build()
+              )
+              .build();
     } catch (Exception e) {
       return PrefillAnswer.invalidFormat();
     }
-
-    return PrefillAnswer.builder()
-        .elementData(
-            ElementData.builder()
-                .id(specification.id())
-                .value(ElementValueBoolean.builder()
-                    .booleanId(configurationRadioBoolean.id())
-                    .value(value)
-                    .build()
-                ).build()
-        ).build();
   }
 
-  public PrefillAnswer prefillAnswer(List<Svar> answers, ElementSpecification specification) {
-    if (!(specification.configuration() instanceof ElementConfigurationRadioBoolean)) {
-      return PrefillAnswer.builder()
-          .errors(List.of(PrefillError.wrongConfigurationType()))
-          .build();
+  private static String getContent(List<Delsvar> subAnswers, List<Svar> answers) {
+    if (!subAnswers.isEmpty()) {
+      return (String) subAnswers.getFirst().getContent().getFirst();
     }
-
-    if (answers.size() != 1) {
-      return PrefillAnswer.builder()
-          .errors(List.of(
-              PrefillError.wrongNumberOfAnswers(specification.id().id(), 1, answers.size())))
-          .build();
-    }
-
-    return prefillSubAnswer(answers.getFirst().getDelsvar(), specification);
-  }
-
-  @Override
-  public List<PrefillAnswer> unknownIds(Svar answer, CertificateModel model) {
-    return Collections.emptyList();
+    return (String) answers.stream()
+        .map(Svar::getDelsvar)
+        .toList()
+        .getFirst()
+        .getFirst()
+        .getContent()
+        .getFirst();
   }
 }
