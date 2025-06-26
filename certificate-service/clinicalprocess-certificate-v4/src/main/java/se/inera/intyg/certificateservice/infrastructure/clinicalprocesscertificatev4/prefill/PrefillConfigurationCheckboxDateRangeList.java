df31ca164ab4d3f2598a344b8cf532e75e6bf9da
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-06-26 09:06:36 +0200

    K1J-1171: WIP PrefillConfigurationCheckboxDateRangeList

--- certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/prefill/PrefillConfigurationCheckboxDateRangeList.java
+++ certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/prefill/PrefillConfigurationCheckboxDateRangeList.java
@@ -1,13 +1,16 @@
 package se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertificatev4.prefill;
 
-import java.time.Period;
 import java.util.List;
 import org.springframework.stereotype.Component;
+import se.inera.intyg.certificateservice.domain.certificate.model.DateRange;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementData;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueDateRangeList;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfiguration;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfigurationCheckboxDateRangeList;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementConfigurationCode;
 import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
+import se.inera.intyg.certificateservice.domain.common.model.Code;
+import se.riv.clinicalprocess.healthcond.certificate.v3.Svar;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Svar.Delsvar;
 import se.riv.clinicalprocess.healthcond.certificate.v33.Forifyllnad;
 
@@ -31,16 +34,41 @@ public class PrefillConfigurationCheckboxDateRangeList implements PrefillConvert
         .filter(svar -> svar.getId().equals(specification.id().id()))
         .toList();
 
-    if (answers.isEmpty()) {
+    final var subAnswers = prefill.getSvar().stream()
+        .map(Svar::getDelsvar)
+        .flatMap(List::stream)
+        .filter(delsvar -> delsvar.getId().equals(specification.id().id()))
+        .toList();
+
+    if (answers.isEmpty() && subAnswers.isEmpty()) {
       return null;
     }
 
-    ElementData elementData = null;
+    ElementData elementData;
 
     try {
+
+      final var content = getContent(subAnswers, answers);
+      final var datePeriodAnswer = PrefillUnmarshaller.datePeriodType(
+          List.of(content)
+      );
+
       elementData = ElementData.builder()
           .id(specification.id())
-          .value(null)
+          .value(ElementValueDateRangeList.builder()
+              .dateRangeListId(configurationCheckboxDateRangeList.id())
+              .dateRangeList(answers.stream()
+                  .map(svar -> {
+                    final var code = getCode(svar.getDelsvar(), specification);
+                    final var dateBox = getDateBox(configurationCheckboxDateRangeList, code);
+
+                    return DateRange.builder()
+                        .dateRangeId(dateBox.id())
+                        .from(PrefillUnmarshaller.toLocalDate(datePeriodAnswer.get().getStart()))
+                        .to(PrefillUnmarshaller.toLocalDate(datePeriodAnswer.get().getEnd()))
+                        .build();
+                  }).toList())
+              .build())
           .build();
 
     } catch (Exception ex) {
@@ -54,33 +82,44 @@ public class PrefillConfigurationCheckboxDateRangeList implements PrefillConvert
         .build();
   }
 
-  private String getName(List<Delsvar> subAnswers, ElementSpecification specification) {
-    return null;
+  private ElementConfigurationCode getDateBox(
+      ElementConfigurationCheckboxDateRangeList configuration,
+      Code code) {
+    return configuration.dateRanges()
+        .stream()
+        .filter(d -> d.code().matches(code))
+        .findFirst().orElseThrow();
   }
 
-  private String getLabel(List<Delsvar> subAnswers, ElementSpecification specification) {
-    return null;
+  private Code getCode(List<Delsvar> subAnswer, ElementSpecification specification) {
+    final var sunAnswer = subAnswer.stream()
+        .filter(d -> d.getId().equals(specification.id().id() + ".1"))
+        .findFirst();
+
+    if (sunAnswer.isEmpty()) {
+      throw new IllegalStateException("Invalid format: code value is missing");
+    }
+
+    final var cvType = PrefillUnmarshaller.cvType(sunAnswer.get().getContent());
+
+    if (cvType.isEmpty()) {
+      throw new IllegalStateException("Invalid format: cvType is empty");
+    }
+
+    var cv = cvType.get();
+    return new Code(cv.getCode(), cv.getCodeSystem(), cv.getDisplayName());
   }
 
-  private String getMessages(List<Delsvar> subAnswers, ElementSpecification specification) {
-    return null;
-  }
-
-  private boolean getHideWorkingHours(List<Delsvar> subAnswers,
-      ElementSpecification specification) {
-    return false;
-  }
-
-  private List<ElementConfigurationCode> getDateRanges(List<Delsvar> subAnswers,
-      ElementSpecification specification) {
-    return null;
-  }
-
-  private Period getMin(List<Delsvar> subAnswers, ElementSpecification specification) {
-    return null;
-  }
-
-  private Period getMax(List<Delsvar> subAnswers, ElementSpecification specification) {
-    return null;
+  private static Object getContent(List<Delsvar> subAnswers, List<Svar> answers) {
+    if (!subAnswers.isEmpty()) {
+      return subAnswers.getFirst().getContent().getFirst();
+    }
+    return answers.stream()
+        .map(Svar::getDelsvar)
+        .toList()
+        .getFirst()
+        .getFirst()
+        .getContent()
+        .getFirst();
   }
 }
