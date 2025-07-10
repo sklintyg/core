348e66f04e7ff71b490d93ca88f0f0e13f3b9e5c
Erik Ågren <erik.agren@soprasteria.com>
2025-07-10 10:32:19 +0200

    K1J-1181: Converter tests

--- certificate-service/app/src/test/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateDataValueConverterDropdownTest.java
+++ certificate-service/app/src/test/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateDataValueConverterDropdownTest.java
@@ -1,6 +1,64 @@
 package se.inera.intyg.certificateservice.application.certificate.service.converter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.inera.intyg.certificateservice.application.certificate.dto.value.CertificateDataValueCode;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueCode;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueDate;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementType;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.FieldId;
+
 class CertificateDataValueConverterDropdownTest {
 
-  //TODO: Implement tests for CertificateDataValueConverterDropdown
+  private static final String CODE_ID = "codeId";
+  private static final String CODE = "code";
+  private CertificateDataValueConverterDropdown certificateDataValueConverterDropdown;
+
+  @BeforeEach
+  void setUp() {
+    certificateDataValueConverterDropdown = new CertificateDataValueConverterDropdown();
+  }
+
+  @Test
+  void shallReturnCorrectType() {
+    assertEquals(ElementType.DROPDOWN, certificateDataValueConverterDropdown.getType());
+  }
+
+  @Test
+  void shallThrowIfWrongType() {
+    final var elementValue = ElementValueDate.builder().build();
+    assertThrows(IllegalStateException.class,
+        () -> certificateDataValueConverterDropdown.convert(null, elementValue));
+  }
+
+  @Test
+  void shallIncludeId() {
+    final var elementValueCode = ElementValueCode.builder()
+        .codeId(new FieldId(CODE_ID))
+        .code(CODE)
+        .build();
+
+    final var certificateDataValue = (CertificateDataValueCode) certificateDataValueConverterDropdown.convert(
+        null,
+        elementValueCode);
+
+    assertEquals(CODE_ID, certificateDataValue.getId());
+  }
+
+  @Test
+  void shallIncludeCode() {
+    final var elementValueCode = ElementValueCode.builder()
+        .codeId(new FieldId(CODE_ID))
+        .code(CODE)
+        .build();
+
+    final var certificateDataValue = (CertificateDataValueCode) certificateDataValueConverterDropdown.convert(
+        null,
+        elementValueCode);
+
+    assertEquals(CODE, certificateDataValue.getCode());
+  }
 }
