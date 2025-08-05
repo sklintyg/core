35bef84b1ca4457163ec3d6148ab6cd12c2eef9c
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-08-05 08:35:59 +0200

    K1J-1236: add model wip

---
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7804/CertificateModelFactoryFK7804.java
@@ -0,0 +1,152 @@
+package se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7804;
+
+import static se.inera.intyg.certificateservice.infrastructure.certificatemodel.common.elements.ElementUnitContactInformation.issuingUnitContactInfo;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import se.inera.intyg.certificateservice.domain.action.certificate.model.CertificateActionFactory;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateMessageType;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModelId;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateType;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateVersion;
+import se.inera.intyg.certificateservice.domain.common.model.CertificateText;
+import se.inera.intyg.certificateservice.domain.common.model.CertificateTextType;
+import se.inera.intyg.certificateservice.domain.common.model.Code;
+import se.inera.intyg.certificateservice.domain.diagnosiscode.repository.DiagnosisCodeRepository;
+import se.inera.intyg.certificateservice.domain.message.model.MessageType;
+import se.inera.intyg.certificateservice.domain.message.model.Subject;
+import se.inera.intyg.certificateservice.infrastructure.certificatemodel.CertificateModelFactory;
+import se.inera.intyg.certificateservice.infrastructure.certificatemodel.common.CertificateRecipientFactory;
+import se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7810.FK7810CertificateActionSpecification;
+import se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7810.FK7810CertificateSummaryProvider;
+import se.inera.intyg.certificateservice.infrastructure.certificatemodel.fk7810.FK7810MessageActionSpecification;
+
+@Component
+@RequiredArgsConstructor
+public class CertificateModelFactoryFK7804 implements CertificateModelFactory {
+
+  private final CertificateActionFactory certificateActionFactory;
+
+  @Value("${certificate.model.fk7804.v2_0.active.from}")
+  private LocalDateTime activeFrom;
+
+  @Value("${sendmessagetofk.logicaladdress}")
+  private String fkLogicalAddress;
+
+  private final DiagnosisCodeRepository diagnosisCodeRepository;
+
+  private static final String FK_7804 = "fk7804";
+  private static final String VERSION = "2.0";
+  private static final String NAME = "Läkarintyg för sjukpenning";
+  private static final String DESCRIPTION = """
+      <b>Vad är sjukpenning?</b>
+      Sjukpenning är en ersättning för personer som arbetar i Sverige och har en nedsatt arbetsförmåga på grund av sjukdom. Beroende på hur mycket arbetsförmågan är nedsatt kan man få en fjärdedels, halv, tre fjärdedels eller hel sjukpenning.\s
+      
+      <b>Andra förmåner som detta läkarintyg används till</b>
+      Om du stänger av patienten enligt smittskyddslagen ska du även använda detta intyg
+      """;
+
+  private static final String DETAILED_DESCRIPTION = """
+      Om du inte känner patienten ska hen styrka sin identitet genom legitimation med foto (HSLF-FS 2018:54).
+      
+      <b>Vad är sjukpenning?</b>
+      Sjukpenning är en ersättning för personer som har en nedsatt arbetsförmåga på grund av sjukdom. Beroende på hur mycket arbetsförmågan är nedsatt kan man få en fjärdedels, halv, tre fjärdedels eller hel sjukpenning.
+      
+      <b>Förutsättningar för att få sjukpenning</b>
+      Arbetsförmågan bedöms i förhållande till personens sysselsättning som kan vara nuvarande arbete eller föräldraledighet för vård av barn. För personer som är arbetslösa bedöms arbetsförmågan i förhållandet till att utföra sådant arbete som är normalt förekommande på arbetsmarknaden.\s
+      
+      För att få sjukpenning ska man, förutom att ha nedsatt arbetsförmåga, tillhöra svensk socialförsäkring och ha inkomst från arbete. Försäkringskassan beslutar om och hur mycket sjukpenning personen kan få.
+      
+      Från den åttonde dagen i en sjukperiod måste det finnas ett läkarintyg. Läkarintyget ska tydligt beskriva hur patientens sjukdom påverkar patientens förmåga att utföra sin sysselsättning.
+      
+      <b>Studerande</b>
+      En person kan få behålla sitt studiemedel om förmågan att studera är helt eller halvt nedsatt på grund av sjukdom. Försäkringskassan bedömer om förmågan är nedsatt och om studieperioden ska godkännas till CSN.
+      
+      Den som ansöker om att få behålla sitt studiemedel ska från och med den femtonde dagen i sjukperioden styrka sin nedsatta studieförmåga med ett läkarintyg.
+      
+      <b>Smittbärarpenning</b>
+      En person kan få ersättning om hen måste avstå från sin sysselsättning på grund av läkarens beslut om avstängning enligt smittskyddslagen eller läkarundersökning alternativt hälsokontroll som syftar till att klarlägga sjukdom, smitta, sår eller annan skada som gör att hen inte får hantera livsmedel.
+      
+      Den som ansöker om smittbärarpenning ska skicka med ett läkarintyg.\s
+      
+      <b>Mer om hur Försäkringskassan bedömer arbetsförmågan</b>
+      Försäkringskassan bedömer arbetsförmågan enligt rehabiliteringskedjan, som innebär följande:
+      
+      Under de första 90 dagarna som personen är sjukskriven kan Försäkringskassan betala sjukpenning om personen inte kan utföra sitt vanliga arbete eller ett annat tillfälligt arbete hos sin arbetsgivare.
+      
+      Efter 90 dagar kan Försäkringskassan betala sjukpenning om personen inte kan utföra något arbete alls hos sin arbetsgivare.
+      
+      Efter 180 dagar kan Försäkringskassan betala ut sjukpenning om personen inte kan utföra sådant arbete som är normalt förekommande på arbetsmarknaden. Men detta gäller inte om Försäkringskassan bedömer att personen med stor sannolikhet kommer att kunna gå tillbaka till ett arbete hos sin arbetsgivare innan dag 366. I dessa fall bedöms arbetsförmågan i förhållande till ett arbete hos arbetsgivaren även efter dag 180. Regeln gäller inte heller om det kan anses oskäligt att bedöma personens arbetsförmåga i förhållande till arbete som är normalt förekommande på arbetsmarknaden.
+      
+      Efter 365 dagar kan Försäkringskassan betala ut sjukpenning om personen inte kan utföra sådant arbete som är normalt förekommande på arbetsmarknaden. Undantag från detta kan göras om det kan anses oskäligt att bedöma personens arbetsförmåga i förhållande till sådant arbete som normalt förekommer på arbetsmarknaden.
+      
+      Rehabiliteringskedjan gäller fullt ut bara för den som har en anställning.
+      
+      Egna företagares arbetsförmåga bedöms i förhållande till de vanliga arbetsuppgifterna fram till och med dag 180. Sedan bedöms arbetsförmågan i förhållande till sådant arbete som normalt förekommer på arbetsmarknaden.
+      
+      För arbetslösa bedöms arbetsförmågan i förhållande till arbeten som normalt förekommer på arbetsmarknaden redan från första dagen i sjukperioden.
+      """;
+
+  private static final String PREAMBLE_TEXT =
+      "";
+
+  public static final CertificateModelId FK7804_V1_0 = CertificateModelId.builder()
+      .type(new CertificateType(FK_7804))
+      .version(new CertificateVersion(VERSION))
+      .build();
+
+  @Override
+  public CertificateModel create() {
+    return CertificateModel.builder()
+        .id(FK7804_V1_0)
+        .type(
+            new Code(
+                "LISJP",
+                "b64ea353-e8f6-4832-b563-fc7d46f29548",
+                NAME
+            )
+        )
+        .name(NAME)
+        .description(DESCRIPTION)
+        .detailedDescription(DETAILED_DESCRIPTION.replaceAll("\\R", ""))
+        .activeFrom(activeFrom)
+        .availableForCitizen(true)
+        .summaryProvider(new FK7810CertificateSummaryProvider())
+        .texts(
+            List.of(
+                CertificateText.builder()
+                    .text(PREAMBLE_TEXT)
+                    .type(CertificateTextType.PREAMBLE_TEXT)
+                    .links(Collections.emptyList())
+                    .build()
+            )
+        )
+        .recipient(CertificateRecipientFactory.fkassa(fkLogicalAddress))
+        .messageTypes(List.of(
+            CertificateMessageType.builder()
+                .type(MessageType.MISSING)
+                .subject(new Subject(MessageType.MISSING.displayName()))
+                .build(),
+            CertificateMessageType.builder()
+                .type(MessageType.CONTACT)
+                .subject(new Subject(MessageType.CONTACT.displayName()))
+                .build(),
+            CertificateMessageType.builder()
+                .type(MessageType.OTHER)
+                .subject(new Subject(MessageType.OTHER.displayName()))
+                .build()
+        ))
+        .certificateActionSpecifications(FK7810CertificateActionSpecification.create())
+        .messageActionSpecifications(FK7810MessageActionSpecification.create())
+        .elementSpecifications(List.of(
+            issuingUnitContactInfo()
+        ))
+        .certificateActionFactory(certificateActionFactory)
+        .build();
+  }
+}
