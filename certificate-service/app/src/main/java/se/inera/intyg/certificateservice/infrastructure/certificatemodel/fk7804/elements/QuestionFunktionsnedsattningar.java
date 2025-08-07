8461fb7096ae685533d8aaa1c2d7096e83201cbc
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-08-07 16:06:59 +0200

    K1J-1240: impl icf wip

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7804/elements/QuestionFunktionsnedsattningar.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/fk7804/elements/QuestionFunktionsnedsattningar.java
@@ -21,7 +21,8 @@ public class QuestionFunktionsnedsattningar {
         .configuration(
             ElementConfigurationIcf.builder()
                 .id(QUESTION_FUNKTIONSNEDSATTNINGAR_FIELD_ID)
-                .name("Ska ha ICF-stöd")
+                .name(
+                    "Ange vilken/vilka funktionsnedsättningar patienten har till följd av sjukdom och om möjligt svårighetsgrad. Ange även vad din bedömning av funktionsnedsättningar baseras på. Beskriv relevanta undersökningsfynd, testresultat, utredningssvar eller andra uppgifter (exempelvis anamnesuppgifter) och hur du bedömer dem.")
                 .build()
         )
         .build();
