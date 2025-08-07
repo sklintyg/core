7ac79226c639c03c8c7fb133c4d23c62b58be6c6
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-08-07 09:02:13 +0200

    K1J-690: Fixed xml generator

--- certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/certificate/XmlGeneratorInteger.java
+++ certificate-service/clinicalprocess-certificate-v4/src/main/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/certificate/XmlGeneratorInteger.java
@@ -10,7 +10,7 @@ import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSp
 import se.riv.clinicalprocess.healthcond.certificate.v3.Svar;
 
 @Component
-public class XmlGeneratorInteger implements XmlGeneratorElementData {
+public class XmlGeneratorInteger implements XmlGeneratorElementValue {
 
   @Override
   public Class<? extends ElementValue> supports() {
