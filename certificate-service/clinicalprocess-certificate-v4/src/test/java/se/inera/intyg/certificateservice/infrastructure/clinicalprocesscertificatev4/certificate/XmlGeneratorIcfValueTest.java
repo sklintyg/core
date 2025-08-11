107333888bd68c14c34e28f821012b5f23d3e97a
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-08-11 09:59:04 +0200

    K1J-1240: add xml generator

---
+++ certificate-service/clinicalprocess-certificate-v4/src/test/java/se/inera/intyg/certificateservice/infrastructure/clinicalprocesscertificatev4/certificate/XmlGeneratorIcfValueTest.java
@@ -0,0 +1,89 @@
+package se.inera.intyg.certificateservice.infrastructure.clinicalprocesscertificatev4.certificate;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementData;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueIcfValue;
+import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueText;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementId;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.FieldId;
+import se.riv.clinicalprocess.healthcond.certificate.v3.Svar;
+import se.riv.clinicalprocess.healthcond.certificate.v3.Svar.Delsvar;
+
+@ExtendWith(MockitoExtension.class)
+class XmlGeneratorIcfValueTest {
+
+  private static final String QUESTION_ID = "QUESTION_ID";
+  private static final String ANSWER_ID = "ANSWER_ID";
+  private static final String TEXT = "TEXT";
+  private final ElementSpecification ELEMENT_SPECIFICATION = ElementSpecification.builder().build();
+
+  @InjectMocks
+  private XmlGeneratorIcfValue xmlGeneratorIcfValue;
+
+  @Test
+  void shouldMapText() {
+    final var data = ElementData.builder()
+        .id(new ElementId(QUESTION_ID))
+        .value(ElementValueIcfValue.builder()
+            .id(new FieldId(ANSWER_ID))
+            .text(TEXT)
+            .build())
+        .build();
+    final var expectedData = new Svar();
+    final var subAnswer = new Delsvar();
+    subAnswer.getContent().add(TEXT);
+    expectedData.setId(QUESTION_ID);
+    subAnswer.setId(ANSWER_ID);
+    expectedData.getDelsvar().add(subAnswer);
+
+    final var response = xmlGeneratorIcfValue.generate(data, ELEMENT_SPECIFICATION);
+
+    assertAll(
+        () -> assertEquals(expectedData.getId(), response.getFirst().getId()),
+        () -> assertEquals(expectedData.getDelsvar().getFirst().getId(),
+            response.getFirst().getDelsvar().getFirst().getId()),
+        () -> assertEquals(expectedData.getDelsvar().getFirst().getContent().getFirst(),
+            response.getFirst().getDelsvar().getFirst().getContent().getFirst())
+    );
+  }
+
+  @Test
+  void shouldMapEmptyIfNullValue() {
+    final var data = ElementData.builder()
+        .value(ElementValueIcfValue.builder()
+            .id(new FieldId(ANSWER_ID))
+            .build()
+        )
+        .id(new ElementId(QUESTION_ID))
+        .build();
+
+    final var response = xmlGeneratorIcfValue.generate(data, ELEMENT_SPECIFICATION);
+
+    assertTrue(response.isEmpty());
+  }
+
+  @Test
+  void shouldMapEmptyIfValueIsNotInteger() {
+    final var data = ElementData.builder()
+        .value(ElementValueText.builder()
+            .text("not an icf value")
+            .textId(new FieldId(ANSWER_ID))
+            .build()
+        )
+        .id(new ElementId(QUESTION_ID))
+        .build();
+
+    final var response = xmlGeneratorIcfValue.generate(data, ELEMENT_SPECIFICATION);
+
+    assertTrue(response.isEmpty());
+  }
+
+}
