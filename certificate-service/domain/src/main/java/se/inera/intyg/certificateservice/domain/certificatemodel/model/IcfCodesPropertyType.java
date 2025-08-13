cec288e5aab1dafdc5ad0b0d0ac5e49849a26f0b
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-08-13 13:54:34 +0200

    K1J-1240: Remove name from IcfCodesPropertyType in domain.

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/IcfCodesPropertyType.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/IcfCodesPropertyType.java
@@ -1,12 +1,5 @@
 package se.inera.intyg.certificateservice.domain.certificatemodel.model;
 
 public enum IcfCodesPropertyType {
-  FUNKTIONSNEDSATTNINGAR("disability"),
-  AKTIVITETSBEGRANSNINGAR("activityLimitation");
-
-  private final String icfCodePropertyName;
-
-  IcfCodesPropertyType(String icfCodePropertyName) {
-    this.icfCodePropertyName = icfCodePropertyName;
-  }
+  FUNKTIONSNEDSATTNINGAR, AKTIVITETSBEGRANSNINGAR
 }
