af1a9aa67a5042541bbc0b8028d2be67a5dafd84
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-08-13 15:54:50 +0200

    K1J-1241: Add enum

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificate/persistence/entity/MessageTypeEnum.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificate/persistence/entity/MessageTypeEnum.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum MessageTypeEnum {
-  COMPLEMENT(1), CONTACT(2), OTHER(3), REMINDER(4), ANSWER(5), MISSING(6);
+  COMPLEMENT(1), CONTACT(2), OTHER(3), REMINDER(4), ANSWER(5), MISSING(6), COORDINATION(7);
 
   private final int key;
 
