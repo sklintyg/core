20b7ee03bcf94e6ec36ca79a0a3a16cf05320d49
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-08-20 13:06:05 +0200

    K1J-1271: Implement Elva77ResponseConverter for response handling and add unit tests for conversion logic

--- intyg-proxy-service/app/src/main/java/se/inera/intyg/intygproxyservice/citizen/service/Elva77ResponseConverter.java
+++ intyg-proxy-service/app/src/main/java/se/inera/intyg/intygproxyservice/citizen/service/Elva77ResponseConverter.java
@@ -1,13 +1,34 @@
 package se.inera.intyg.intygproxyservice.citizen.service;
 
 import org.springframework.stereotype.Component;
+import se.inera.intyg.intygproxyservice.citizen.dto.CitizenDTO;
 import se.inera.intyg.intygproxyservice.citizen.dto.CitizenResponse;
 import se.inera.intyg.intygproxyservice.integration.api.elva77.Elva77Response;
+import se.inera.intyg.intygproxyservice.integration.api.pu.Status;
 
 @Component
 public class Elva77ResponseConverter {
 
   public CitizenResponse convert(Elva77Response elva77Response) {
-    return null;
+    return switch (elva77Response.getResult()) {
+      case ERROR -> CitizenResponse.builder().status(Status.ERROR).build();
+      case OK, INFO -> {
+        final var citizen = elva77Response.getCitizen();
+        yield CitizenResponse.builder()
+            .citizen(
+                CitizenDTO.builder()
+                    .personnummer(citizen.getSubjectOfCareId())
+                    .fornamn(citizen.getFirstname())
+                    .efternamn(citizen.getLastname())
+                    .postort(citizen.getCity())
+                    .postadress(citizen.getStreetAddress())
+                    .postnummer(citizen.getZip())
+                    .isActive(citizen.getIsActive())
+                    .build()
+            )
+            .status(Status.FOUND)
+            .build();
+      }
+    };
   }
 }
--- intyg-proxy-service/integration-api/src/main/java/se/inera/intyg/intygproxyservice/integration/api/elva77/Result.java
+++ intyg-proxy-service/integration-api/src/main/java/se/inera/intyg/intygproxyservice/integration/api/elva77/Result.java
@@ -1,14 +1,5 @@
 package se.inera.intyg.intygproxyservice.integration.api.elva77;
 
-import se.inera.intyg.intygproxyservice.integration.api.pu.Status;
-
 public enum Result {
-  OK, INFO, ERROR;
-
-  public Status toStatus() {
-    return switch (this) {
-      case OK, INFO -> Status.FOUND;
-      case ERROR -> Status.ERROR;
-    };
-  }
+  OK, INFO, ERROR
 }
