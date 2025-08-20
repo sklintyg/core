1f35d184144a23af6ecc48e6be208cbbcf333e6f
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-08-20 16:06:39 +0200

    K1J-1271: Add @JsonIgnoreProperties(ignoreUnknown = true) to handle parsed persons without isActive field set.

--- intyg-proxy-service/integration-fakepu/src/main/java/se/inera/intyg/intygproxyservice/integration/fakepu/repository/model/ParsedPerson.java
+++ intyg-proxy-service/integration-fakepu/src/main/java/se/inera/intyg/intygproxyservice/integration/fakepu/repository/model/ParsedPerson.java
@@ -1,5 +1,6 @@
 package se.inera.intyg.intygproxyservice.integration.fakepu.repository.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ParsedPerson {
 
   ParsedPersonalIdentity personalIdentity;
