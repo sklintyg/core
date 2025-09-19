4c27e72d8ec6b62de1ccf62cb5501b85e4170093
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-09-19 10:41:40 +0200

    K1J-1202: Fixed test

--- certificate-service/domain/src/test/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/ElementConfigurationDiagnosisTest.java
+++ certificate-service/domain/src/test/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/ElementConfigurationDiagnosisTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementSimplifiedValueTable;
@@ -11,6 +12,31 @@ import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueDi
 import se.inera.intyg.certificateservice.domain.certificate.model.ElementValueDiagnosisList;
 
 class ElementConfigurationDiagnosisTest {
+
+  private static final String ID = "id";
+
+  @Test
+  void shallReturnEmptyValue() {
+    final var expectedValue = ElementValueDiagnosisList.builder()
+        .diagnoses(Collections.emptyList())
+        .build();
+
+    final var configurationDiagnosis = ElementConfigurationDiagnosis.builder().build();
+    assertEquals(expectedValue, configurationDiagnosis.emptyValue());
+  }
+
+  @Test
+  void shallReturnCodeSystem() {
+    final var expectedCodeSystem = "expectedCodeSystem";
+    final var configurationDiagnosis = ElementConfigurationDiagnosis.builder()
+        .terminology(
+            List.of(
+                new ElementDiagnosisTerminology(ID, "label", expectedCodeSystem)
+            )
+        )
+        .build();
+    assertEquals(expectedCodeSystem, configurationDiagnosis.codeSystem(ID));
+  }
 
   @Test
   void simplifiedShouldReturnTableWithHeadingsAndRows() {
