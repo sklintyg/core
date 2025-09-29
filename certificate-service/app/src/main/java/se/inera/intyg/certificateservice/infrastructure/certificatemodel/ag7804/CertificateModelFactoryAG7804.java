1364bd60abdf775e524aa154a6e9e2a7a106a9b5
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-09-29 14:49:04 +0200

    K1J-1201: Add smittbararpenning cat & test

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/ag7804/CertificateModelFactoryAG7804.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/ag7804/CertificateModelFactoryAG7804.java
@@ -1,5 +1,7 @@
 package se.inera.intyg.certificateservice.infrastructure.certificatemodel.ag7804;
 
+import static se.inera.intyg.certificateservice.infrastructure.certificatemodel.ag7804.elemets.CategorySmittbararpenning.categorySmittbararpenning;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -30,24 +32,22 @@ public class CertificateModelFactoryAG7804 implements CertificateModelFactory {
   private static final String NAME = "Läkarintyg om arbetsförmåga – arbetsgivaren";
   private static final String DESCRIPTION = """
       Läkarintyg om arbetsförmåga – arbetsgivaren skapas från Försäkringskassans läkarintyg för sjukpenning (FK 7804) och används i följande situationer:
-      <ul>
-        <li>När patienten har en anställning och behöver ett läkarintyg i förhållande till sin arbetsgivare.</li>
-        <li>När patienten varit frånvarande från arbetet i fler än 14 dagar.</li>
-        <li>När intygsutfärdande läkaren bedömer att patienten behöver vara sjukfrånvarande längre tid än 14 dagar och att sjukfrånvaron därmed kommer att överstiga sjuklöneperioden. <em>Läkarintyg om arbetsförmåga – arbetsgivaren</em> kan utfärdas för att undvika ett återbesök efter dag 14 och fyller då funktionen som intyg till arbetsgivare både <em>under</em> sjuklöneperioden och <em>efter</em> sjuklöneperioden.</li>
-      </ul>
+      
+      <ul><li>När patienten har en anställning och behöver ett läkarintyg i förhållande till sin arbetsgivare.</li><li>När patienten varit frånvarande från arbetet i fler än 14 dagar.</li><li>När intygsutfärdande läkaren bedömer att patienten behöver vara sjukfrånvarande längre tid än 14 dagar och att sjukfrånvaron därmed kommer att överstiga sjuklöneperioden. <em>Läkarintyg om arbetsförmåga – arbetsgivaren</em> kan utfärdas för att undvika ett återbesök efter dag 14 och fyller då funktionen som intyg till arbetsgivare både <em>under</em> sjuklöneperioden och <em>efter</em> sjuklöneperioden.</li></ul>
       """;
 
   private static final String DETAILED_DESCRIPTION = """
       <p><b>Utfärdare</b><br/>Läkarintyget får utfärdas av läkare eller tandläkare. I intyget omfattar begreppet läkare båda professionerna. Det är inte möjligt att delegera rätten att utfärda läkarintyget till annan profession, till exempel sjuksköterska eller fysioterapeut. Se även Socialstyrelsens föreskrifter om intyg (HSLF-FS 2018:54).</p>
       <p><b>Syften med läkarintyget</b></p>
       <ul>
-        <li><p><b>Rätt till frånvaro.</b> Att ge arbetstagaren rätt att vara frånvarande från arbetet på grund av sjukdom/skada. Arbetstagaren är skyldig att vid sjukdom/skada lämna in ett medicinskt underlag som styrker dels att frånvaron beror på sjukdom, dels att sjukdomen sätter ned arbetsförmågan i arbetstagarens arbetsuppgifter. Om läkarintyget inte ger tillräcklig information kan frånvaron anses vara olovlig och medföra att intyget behöver kompletteras.</p></ul>
-        <li><p><b>Arbetsgivarens rehabiliteringsansvar.</b> Läkarintyget ska ge arbetsgivaren tillräcklig information om arbetstagarens medicinska hinder för att utföra sina ordinarie arbetsuppgifter. Arbetsgivare har enligt socialförsäkringsbalken, arbetsmiljölagen och arbetsdomstolens rättspraxis ett omfattande och långtgående rehabiliteringsansvar och ska genom skäliga stöd och anpassningar på arbetsplatsen underlätta för sjuka eller skadade att komma tillbaka i arbete.</p></ul>
-        <li><p><b>Plan för återgång i arbete.</b> Sedan 2018 har arbetsgivare en skyldighet enligt 30 kap. 6 § socialförsäkringsbalken att ta fram en plan för återgång i arbete när en arbetstagare förväntas vara sjuk under en längre tid. Planen ska kontinuerligt följas upp och revideras vid behov. Planen är främst ett stöd för rehabiliteringsarbetet på arbetsplatsen och ska underlätta en tidig återgång i arbete. Läkarintyget ger grundläggande medicinsk information om patientens möjligheter att återgå i arbete och är en förutsättning för att arbetsgivaren tillsammans med sin arbetstagare ska kunna upprätta en plan för återgång i arbete.</p></ul>
-        <li><p><b>Sjukpenningtillägg.</b> Att ge arbetstagaren rätt till kompletterande sjukpenningtillägg. De flesta kollektivavtal på svensk arbetsmarknad ger arbetstagare rätt till sjukpenningtillägg vid sjukdom.</p></ul>
-        <li><p><b>Läkarintyg från och med dag 1 i sjuklöneperioden</b> Läkarintyget kan användas från och med dag 1 i sjuklöneperioden, ett så kallat förstadagsintyg. Arbetsgivaren får om det finns särskilda skäl för det begära att arbetstagaren genom intyg av läkare styrker nedsättning av arbetsförmågan tidigare än dag 8 i sjuklöneperioden.</p></ul>
+        <li><p><b>Rätt till frånvaro.</b> Att ge arbetstagaren rätt att vara frånvarande från arbetet på grund av sjukdom/skada. Arbetstagaren är skyldig att vid sjukdom/skada lämna in ett medicinskt underlag som styrker dels att frånvaron beror på sjukdom, dels att sjukdomen sätter ned arbetsförmågan i arbetstagarens arbetsuppgifter. Om läkarintyget inte ger tillräcklig information kan frånvaron anses vara olovlig och medföra att intyget behöver kompletteras.</p></li>
+        <li><p><b>Arbetsgivarens rehabiliteringsansvar.</b> Läkarintyget ska ge arbetsgivaren tillräcklig information om arbetstagarens medicinska hinder för att utföra sina ordinarie arbetsuppgifter. Arbetsgivare har enligt socialförsäkringsbalken, arbetsmiljölagen och arbetsdomstolens rättspraxis ett omfattande och långtgående rehabiliteringsansvar och ska genom skäliga stöd och anpassningar på arbetsplatsen underlätta för sjuka eller skadade att komma tillbaka i arbete.</p></li>
+        <li><p><b>Plan för återgång i arbete.</b> Sedan 2018 har arbetsgivare en skyldighet enligt 30 kap. 6 § socialförsäkringsbalken att ta fram en plan för återgång i arbete när en arbetstagare förväntas vara sjuk under en längre tid. Planen ska kontinuerligt följas upp och revideras vid behov. Planen är främst ett stöd för rehabiliteringsarbetet på arbetsplatsen och ska underlätta en tidig återgång i arbete. Läkarintyget ger grundläggande medicinsk information om patientens möjligheter att återgå i arbete och är en förutsättning för att arbetsgivaren tillsammans med sin arbetstagare ska kunna upprätta en plan för återgång i arbete.</p></li>
+        <li><p><b>Sjukpenningtillägg.</b> Att ge arbetstagaren rätt till kompletterande sjukpenningtillägg. De flesta kollektivavtal på svensk arbetsmarknad ger arbetstagare rätt till sjukpenningtillägg vid sjukdom.</p></li>
+        <li><p><b>Läkarintyg från och med dag 1 i sjuklöneperioden</b> Läkarintyget kan användas från och med dag 1 i sjuklöneperioden, ett så kallat förstadagsintyg. Arbetsgivaren får om det finns särskilda skäl för det begära att arbetstagaren genom intyg av läkare styrker nedsättning av arbetsförmågan tidigare än dag 8 i sjuklöneperioden.</p></li>
       </ul>
-      <p><b>Innehåll i läkarintyget</b> Läkarintyg om arbetsförmåga - arbetsgivaren skapas från Försäkringskassans läkarintyg för sjukpenning (FK 7804) och ska innehålla motsvarande information.  Patienten kan dock välja om hen vill delge arbetsgivaren information om vilken eller vilka diagnoser som föreligger.  Övrig information i intyget är obligatorisk.</p>
+      <p><b>Innehåll i läkarintyget</b></p> 
+      <p>Läkarintyg om arbetsförmåga - arbetsgivaren skapas från Försäkringskassans läkarintyg för sjukpenning (FK 7804) och ska innehålla motsvarande information.  Patienten kan dock välja om hen vill delge arbetsgivaren information om vilken eller vilka diagnoser som föreligger.  Övrig information i intyget är obligatorisk.</p>
       <p><b>När ska Läkarintyg om arbetsförmåga – arbetsgivaren användas?</b></p>
       <ul>
         <li>När patienten har en anställning och behöver ett läkarintyg i förhållande till sin arbetsgivare.</li>
@@ -81,7 +81,9 @@ public class CertificateModelFactoryAG7804 implements CertificateModelFactory {
         .summaryProvider(new AG7804CertificateSummaryProvider())
         .certificateActionSpecifications(AG7804CertificateActionSpecification.create())
         .messageActionSpecifications(AG7804MessageActionSpecification.create())
-        .elementSpecifications(List.of())
+        .elementSpecifications(List.of(
+            categorySmittbararpenning()
+        ))
         .certificateActionFactory(certificateActionFactory)
         .build();
   }
