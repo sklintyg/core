c59c90993b33e23c24f6525da695dfe1a29dc535
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-09-29 12:45:32 +0200

    K1J-1423: Fixed test

--- certificate-analytics-service/app/src/test/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/entity/mapper/v1/CertificateEntityMapperV1Test.java
+++ certificate-analytics-service/app/src/test/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/entity/mapper/v1/CertificateEntityMapperV1Test.java
@@ -12,13 +12,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.CareProviderEntity;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.CertificateEntity;
+import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.CertificateTypeEntity;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.PatientEntity;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.UnitEntity;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.repository.CareProviderRepository;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.repository.CertificateEntityRepository;
+import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.repository.CertificateTypeRepository;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.repository.PatientRepository;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.repository.UnitRepository;
-import se.inera.intyg.certificateanalyticsservice.testdata.TestDataEntities;
 import se.inera.intyg.certificateanalyticsservice.testdata.TestDataMessages;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,32 +34,37 @@ class CertificateEntityMapperV1Test {
   @Mock
   private PatientRepository patientRepository;
   @Mock
+  private CertificateTypeRepository certificateTypeRepository;
+  @Mock
   private CertificateEntityRepository certificateEntityRepository;
 
   @Test
   void shouldMapCertificateCorrectly() {
-    final var certificateMsg = TestDataMessages.certificate();
+    final var message = TestDataMessages.certificate();
     final var expectedCareProvider = mock(CareProviderEntity.class);
     final var expectedUnit = mock(UnitEntity.class);
     final var expectedPatient = mock(PatientEntity.class);
-    final var expectedCertificateType = TestDataEntities.certificateEntity().getCertificateType();
+    final var expectedCertificateType = mock(CertificateTypeEntity.class);
 
-    when(certificateEntityRepository.findByCertificateId(certificateMsg.getId())).thenReturn(
+    when(certificateEntityRepository.findByCertificateId(message.getId())).thenReturn(
         Optional.empty());
-    when(careProviderRepository.findOrCreate(certificateMsg.getCareProviderId())).thenReturn(
+    when(careProviderRepository.findOrCreate(message.getCareProviderId())).thenReturn(
         expectedCareProvider);
-    when(unitRepository.findOrCreate(certificateMsg.getUnitId())).thenReturn(expectedUnit);
-    when(patientRepository.findOrCreate(certificateMsg.getPatientId())).thenReturn(expectedPatient);
+    when(unitRepository.findOrCreate(message.getUnitId())).thenReturn(expectedUnit);
+    when(patientRepository.findOrCreate(message.getPatientId())).thenReturn(expectedPatient);
+    when(certificateTypeRepository.findOrCreate(message.getType(),
+        message.getTypeVersion())).thenReturn(
+        expectedCertificateType);
 
     final var expected = CertificateEntity.builder()
-        .certificateId(certificateMsg.getId())
+        .certificateId(message.getId())
         .certificateType(expectedCertificateType)
         .patient(expectedPatient)
         .unit(expectedUnit)
         .careProvider(expectedCareProvider)
         .build();
 
-    final var result = certificateEntityMapperV1.map(certificateMsg);
+    final var result = certificateEntityMapperV1.map(message);
 
     assertEquals(expected, result);
   }
--- certificate-analytics-service/app/src/test/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/repository/JpaCertificateAnalyticsEventRepositoryTest.java
+++ certificate-analytics-service/app/src/test/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/repository/JpaCertificateAnalyticsEventRepositoryTest.java
@@ -4,13 +4,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.EventEntity;
-import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.mapper.v1.EventMapperV1;
+import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.mapper.v1.EventMapper;
 import se.inera.intyg.certificateanalyticsservice.testdata.TestDataMessages;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,19 +19,24 @@ class JpaCertificateAnalyticsEventRepositoryTest {
   @Mock
   private EventEntityRepository eventEntityRepository;
   @Mock
-  private EventMapperV1 eventMapperV1;
-
-  @InjectMocks
-  private JpaCertificateAnalyticsEventRepository jpaCertificateAnalyticsEventRepository;
+  private EventMapper eventMapper;
 
   @Test
   void shouldMapAndSaveCreatedEventMessage() {
     final var message = TestDataMessages.CREATED_EVENT_MESSAGE;
-    final var entityToSave = eventMapperV1.toEntity(TestDataMessages.CREATED_EVENT_MESSAGE);
-    when(eventEntityRepository.save(entityToSave)).thenReturn(mock(EventEntity.class));
+    final var entityToSave = mock(EventEntity.class);
+    when(eventMapper.supports(message)).thenReturn(true);
+    when(eventMapper.toEntity(message)).thenReturn(entityToSave);
 
-    jpaCertificateAnalyticsEventRepository.save(message);
+    JpaCertificateAnalyticsEventRepository repo = new JpaCertificateAnalyticsEventRepository(
+        eventEntityRepository, List.of(eventMapper));
 
+    when(eventEntityRepository.save(entityToSave)).thenReturn(entityToSave);
+
+    repo.save(message);
+
+    verify(eventMapper).supports(message);
+    verify(eventMapper).toEntity(message);
     verify(eventEntityRepository).save(entityToSave);
   }
 }
