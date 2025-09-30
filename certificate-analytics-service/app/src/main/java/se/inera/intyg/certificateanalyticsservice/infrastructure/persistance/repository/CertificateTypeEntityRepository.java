f642fd1d61f0e4ad0961efa998228e5c376b340a
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-09-30 08:25:53 +0200

    K1J-1423: Addressed comments in PR

--- certificate-analytics-service/app/src/main/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/repository/CertificateTypeEntityRepository.java
+++ certificate-analytics-service/app/src/main/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/repository/CertificateTypeEntityRepository.java
@@ -2,8 +2,10 @@ package se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.re
 
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.CertificateTypeEntity;
 
+@Repository
 public interface CertificateTypeEntityRepository extends
     CrudRepository<CertificateTypeEntity, Long> {
 
--- certificate-analytics-service/app/src/main/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/repository/EventEntityRepository.java
+++ certificate-analytics-service/app/src/main/java/se/inera/intyg/certificateanalyticsservice/infrastructure/persistance/repository/EventEntityRepository.java
@@ -1,8 +1,10 @@
 package se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import se.inera.intyg.certificateanalyticsservice.infrastructure.persistance.entity.EventEntity;
 
+@Repository
 public interface EventEntityRepository extends CrudRepository<EventEntity, Integer> {
 
 }
