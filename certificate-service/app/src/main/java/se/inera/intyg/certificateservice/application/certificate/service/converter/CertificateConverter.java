ad8f4d432b9b6b0b7ba6c58065d92b67eb20c68e
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-10-01 11:51:05 +0200

    K1J-513: Implement visibility configuration handling for draft certificates with complement requests.

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateConverter.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateConverter.java
@@ -4,9 +4,12 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.application.certificate.dto.CertificateDTO;
+import se.inera.intyg.certificateservice.application.certificate.service.HandleComplementElementVisibilityService;
 import se.inera.intyg.certificateservice.application.common.dto.ResourceLinkDTO;
 import se.inera.intyg.certificateservice.domain.action.certificate.model.ActionEvaluation;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
+import se.inera.intyg.certificateservice.domain.certificate.model.RelationType;
+import se.inera.intyg.certificateservice.domain.message.model.Message;
 
 @Component
 @RequiredArgsConstructor
@@ -14,16 +17,32 @@ public class CertificateConverter {
 
   private final CertificateDataConverter certificateDataConverter;
   private final CertificateMetadataConverter certificateMetadataConverter;
+  private final HandleComplementElementVisibilityService handleComplementElementVisibilityService;
 
   public CertificateDTO convert(Certificate certificate, List<ResourceLinkDTO> resourceLinks,
       ActionEvaluation actionEvaluation) {
+    var certificateDataElementMap = certificateDataConverter.convert(certificate, false);
+
+    if (draftWithComplementRequestOnParent(certificate)) {
+      certificate.parent().certificate().messages().stream()
+          .map(Message::complements)
+          .flatMap(List::stream)
+          .forEach(complement ->
+              certificate.certificateModel()
+                  .elementSpecification(complement.elementId())
+                  .getVisibilityConfiguration()
+                  .ifPresent(
+                      config -> handleComplementElementVisibilityService.handle(
+                          complement.elementId(),
+                          certificateDataElementMap,
+                          certificate,
+                          config
+                      )));
+    }
+
     return CertificateDTO.builder()
-        .metadata(
-            certificateMetadataConverter.convert(certificate, actionEvaluation)
-        )
-        .data(
-            certificateDataConverter.convert(certificate, false)
-        )
+        .metadata(certificateMetadataConverter.convert(certificate, actionEvaluation))
+        .data(certificateDataElementMap)
         .links(resourceLinks)
         .build();
   }
@@ -31,14 +50,14 @@ public class CertificateConverter {
   public CertificateDTO convertForCitizen(Certificate certificate,
       List<ResourceLinkDTO> resourceLinks) {
     return CertificateDTO.builder()
-        .metadata(
-            certificateMetadataConverter.convert(certificate, null)
-        )
-        .data(
-            certificateDataConverter.convert(certificate, true)
-        )
+        .metadata(certificateMetadataConverter.convert(certificate, null))
+        .data(certificateDataConverter.convert(certificate, true))
         .links(resourceLinks)
         .build();
   }
 
+  private static boolean draftWithComplementRequestOnParent(Certificate certificate) {
+    return certificate.isDraft() && certificate.parent() != null && certificate.parent().type()
+        .equals(RelationType.COMPLEMENT);
+  }
 }
--- certificate-service/app/src/test/java/se/inera/intyg/certificateservice/application/certificate/service/CertificateConverterTest.java
+++ certificate-service/app/src/test/java/se/inera/intyg/certificateservice/application/certificate/service/CertificateConverterTest.java
@@ -2,6 +2,8 @@ package se.inera.intyg.certificateservice.application.certificate.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,6 +24,15 @@ import se.inera.intyg.certificateservice.application.common.dto.ResourceLinkDTO;
 import se.inera.intyg.certificateservice.domain.action.certificate.model.ActionEvaluation;
 import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
 import se.inera.intyg.certificateservice.domain.certificate.model.MedicalCertificate;
+import se.inera.intyg.certificateservice.domain.certificate.model.Relation;
+import se.inera.intyg.certificateservice.domain.certificate.model.RelationType;
+import se.inera.intyg.certificateservice.domain.certificate.model.Status;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.CertificateModel;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementId;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementSpecification;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementVisibilityConfigurationsCheckboxMultipleCode;
+import se.inera.intyg.certificateservice.domain.message.model.Complement;
+import se.inera.intyg.certificateservice.domain.message.model.Message;
 
 @ExtendWith(MockitoExtension.class)
 class CertificateConverterTest {
@@ -31,6 +42,9 @@ class CertificateConverterTest {
   private Certificate certificate;
   private static final String KEY = "key";
 
+
+  @Mock
+  private HandleComplementElementVisibilityService handleComplementElementVisibilityService;
   @Mock
   private CertificateMetadataConverter certificateMetadataConverter;
   @Mock
@@ -141,6 +155,171 @@ class CertificateConverterTest {
         assertEquals(expectedLinks,
             certificateConverter.convertForCitizen(certificate, expectedLinks).getLinks());
       }
+    }
+  }
+
+  @Nested
+  class VisibilityConfigurationTests {
+
+    @Test
+    void shallHandleVisibilityConfigurationIfCertificateIsDraftWithComplementOnParent() {
+      final var elementId = new ElementId("id");
+
+      final var visibilityConfiguration = ElementVisibilityConfigurationsCheckboxMultipleCode
+          .builder()
+          .build();
+
+      final var medicalCertificate = MedicalCertificate.builder()
+          .status(Status.DRAFT)
+          .certificateModel(
+              CertificateModel.builder()
+                  .elementSpecifications(
+                      List.of(
+                          ElementSpecification.builder()
+                              .id(elementId)
+                              .visibilityConfiguration(
+                                  visibilityConfiguration
+                              )
+                              .build()
+                      )
+                  )
+                  .build()
+          )
+          .parent(
+              Relation.builder()
+                  .type(RelationType.COMPLEMENT)
+                  .certificate(
+                      MedicalCertificate.builder()
+                          .messages(
+                              List.of(
+                                  Message.builder()
+                                      .complements(
+                                          List.of(
+                                              Complement.builder()
+                                                  .elementId(elementId)
+                                                  .build()
+                                          )
+                                      )
+                                      .build()
+                              )
+                          )
+                          .build()
+                  )
+                  .build()
+          )
+          .build();
+
+      final var dataElementMap = Map.of(KEY, CertificateDataElement.builder().build());
+
+      doReturn(dataElementMap).when(certificateDataConverter)
+          .convert(medicalCertificate, false);
+
+      certificateConverter.convert(medicalCertificate, resourceLinkDTOs, ACTION_EVALUATION);
+
+      verify(handleComplementElementVisibilityService).handle(
+          elementId,
+          dataElementMap,
+          medicalCertificate,
+          visibilityConfiguration
+      );
+    }
+
+    @Test
+    void shallNotHandleVisibilityConfigurationIfCertificateIsDraftWithoutComplementOnParent() {
+      final var elementId = new ElementId("id");
+
+      final var visibilityConfiguration = ElementVisibilityConfigurationsCheckboxMultipleCode
+          .builder()
+          .build();
+
+      final var medicalCertificate = MedicalCertificate.builder()
+          .status(Status.DRAFT)
+          .certificateModel(
+              CertificateModel.builder()
+                  .elementSpecifications(
+                      List.of(
+                          ElementSpecification.builder()
+                              .id(elementId)
+                              .visibilityConfiguration(
+                                  visibilityConfiguration
+                              )
+                              .build()
+                      )
+                  )
+                  .build()
+          )
+          .build();
+
+      final var dataElementMap = Map.of(KEY, CertificateDataElement.builder().build());
+
+      doReturn(dataElementMap).when(certificateDataConverter)
+          .convert(medicalCertificate, false);
+
+      certificateConverter.convert(medicalCertificate, resourceLinkDTOs, ACTION_EVALUATION);
+
+      verifyNoInteractions(handleComplementElementVisibilityService);
+    }
+
+    @Test
+    void shallNotHandleVisibilityConfigurationIfCertificateIsDraftWithComplementOnParentWithoutVisibilityConfiguration() {
+      final var elementId = new ElementId("id");
+
+      final var medicalCertificate = MedicalCertificate.builder()
+          .status(Status.DRAFT)
+          .certificateModel(
+              CertificateModel.builder()
+                  .elementSpecifications(
+                      List.of(
+                          ElementSpecification.builder()
+                              .id(elementId)
+                              .build()
+                      )
+                  )
+                  .build()
+          )
+          .parent(
+              Relation.builder()
+                  .type(RelationType.COMPLEMENT)
+                  .certificate(
+                      MedicalCertificate.builder()
+                          .messages(
+                              List.of(
+                                  Message.builder()
+                                      .complements(
+                                          List.of(
+                                              Complement.builder()
+                                                  .elementId(elementId)
+                                                  .build()
+                                          )
+                                      )
+                                      .build()
+                              )
+                          )
+                          .build()
+                  )
+                  .build()
+          )
+          .build();
+
+      final var dataElementMap = Map.of(KEY, CertificateDataElement.builder().build());
+
+      doReturn(dataElementMap).when(certificateDataConverter)
+          .convert(medicalCertificate, false);
+
+      certificateConverter.convert(medicalCertificate, resourceLinkDTOs, ACTION_EVALUATION);
+
+      verifyNoInteractions(handleComplementElementVisibilityService);
+    }
+
+    @Test
+    void shallNotHandleVisibilityConfigurationIfCertificateIsNotDraft() {
+      final var medicalCertificate = MedicalCertificate.builder()
+          .status(Status.SIGNED)
+          .build();
+
+      certificateConverter.convert(medicalCertificate, resourceLinkDTOs, ACTION_EVALUATION);
+
+      verifyNoInteractions(handleComplementElementVisibilityService);
     }
   }
 }
