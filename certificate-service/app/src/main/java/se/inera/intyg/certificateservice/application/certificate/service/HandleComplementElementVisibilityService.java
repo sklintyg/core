46f77eafa165a7a2e3f596ae67a6cdc16f7d2df3
cigerhed <christoffer.sigerhed@soprasteria.com>
2025-10-01 12:10:02 +0200

    K1J-513: Add HandleComplementElementVisibilityService to manage visibility configurations for complement elements

---
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/HandleComplementElementVisibilityService.java
@@ -0,0 +1,46 @@
+package se.inera.intyg.certificateservice.application.certificate.service;
+
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se.inera.intyg.certificateservice.application.certificate.dto.CertificateDataElement;
+import se.inera.intyg.certificateservice.domain.certificate.model.Certificate;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementId;
+import se.inera.intyg.certificateservice.domain.certificatemodel.model.ElementVisibilityConfiguration;
+
+@Service
+@RequiredArgsConstructor
+public class HandleComplementElementVisibilityService {
+
+  private final List<ComplementElementVisibility> complementElementVisibility;
+
+  public void handle(ElementId complementElementId,
+      Map<String, CertificateDataElement> dataElementMap,
+      Certificate certificate, ElementVisibilityConfiguration visibilityConfiguration) {
+    if (elementIsVisible(certificate, complementElementId)) {
+      return;
+    }
+
+    final var elementVisibility = complementElementVisibility.stream()
+        .filter(visibilityService -> visibilityService.supports(visibilityConfiguration))
+        .findFirst()
+        .orElseThrow(
+            () -> new IllegalStateException("Complement element visibility service not found")
+        );
+
+    elementVisibility.handle(dataElementMap, certificate, visibilityConfiguration);
+  }
+
+  private static boolean elementIsVisible(Certificate certificate, ElementId elementId) {
+    final var shouldValidate = certificate.certificateModel()
+        .elementSpecification(elementId)
+        .shouldValidate();
+
+    if (shouldValidate == null) {
+      return true;
+    }
+
+    return shouldValidate.test(certificate.elementData());
+  }
+}
