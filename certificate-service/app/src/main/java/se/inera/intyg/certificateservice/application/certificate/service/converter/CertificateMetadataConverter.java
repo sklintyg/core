e09bd79d3d2c47480adcba07d7c3e9241f7fe037
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-10-06 10:16:01 +0200

    K1J-1201: Fix: Return null for CertificateRecipientDTO when recipient is null

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateMetadataConverter.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/application/certificate/service/converter/CertificateMetadataConverter.java
@@ -196,7 +196,7 @@ public class CertificateMetadataConverter {
   private CertificateRecipientDTO toCertificateRecipientDTO(Certificate certificate) {
     final var sent = certificate.sent();
     if (certificate.certificateModel().recipient() == null) {
-      return CertificateRecipientDTO.builder().build();
+      return null;
     }
     if (sent == null) {
       return CertificateRecipientDTO.builder()
