517f156e624b4f41d9b642d037b7f341180f640e
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-10-07 16:42:20 +0200

    K1J-1460: General print WIP

--- certificate-service/integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/dto/ElementSimplifiedValueLabeledTextDTO.java
+++ certificate-service/integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/dto/ElementSimplifiedValueLabeledTextDTO.java
@@ -1,10 +1,12 @@
 package se.inera.intyg.certificateservice.certificate.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
+@JsonTypeName("LABELED_TEXT")
 public class ElementSimplifiedValueLabeledTextDTO implements ElementSimplifiedValueDTO {
 
   String label;
--- certificate-service/integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/dto/ElementSimplifiedValueTextDTO.java
+++ certificate-service/integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/dto/ElementSimplifiedValueTextDTO.java
@@ -1,11 +1,13 @@
 package se.inera.intyg.certificateservice.certificate.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Builder;
 import lombok.Value;
 import lombok.With;
 
 @Value
 @Builder
+@JsonTypeName("TEXT")
 public class ElementSimplifiedValueTextDTO implements ElementSimplifiedValueDTO {
 
   @With
