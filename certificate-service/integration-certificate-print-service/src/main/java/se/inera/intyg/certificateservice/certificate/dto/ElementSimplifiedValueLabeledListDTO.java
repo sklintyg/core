5992a144b1102e791af4931517cda3ca4b3c40a1
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-10-09 08:38:58 +0200

    K1J-1460: Removed unnecessary json annotation

--- certificate-service/integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/dto/ElementSimplifiedValueLabeledListDTO.java
+++ certificate-service/integration-certificate-print-service/src/main/java/se/inera/intyg/certificateservice/certificate/dto/ElementSimplifiedValueLabeledListDTO.java
@@ -1,6 +1,5 @@
 package se.inera.intyg.certificateservice.certificate.dto;
 
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.util.List;
 import lombok.Builder;
 import lombok.Value;
@@ -8,7 +7,6 @@ import lombok.With;
 
 @Value
 @Builder
-@JsonTypeName("LABELED_LIST")
 public class ElementSimplifiedValueLabeledListDTO implements ElementSimplifiedValueDTO {
 
   @With
