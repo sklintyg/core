84601fe9a2a4b0db911c9c5ba3c9a5d6f0db8ec0
Erik Ågren <erik.agren@soprasteria.com>
2025-10-10 09:24:55 +0200

    K1J-1437: cleanup

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/unit/model/CareUnit.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/unit/model/CareUnit.java
@@ -1,7 +1,6 @@
 package se.inera.intyg.certificateservice.domain.unit.model;
 
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 import se.inera.intyg.certificateservice.domain.common.model.HsaId;
 
