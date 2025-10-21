b86fa7e493c91598aad1d5f2f42c8d7138bc18fe
Lucas Lundström <lucas.lundstroem@soprasteria.com>
2025-10-21 23:23:59 +0200

    K1J-1500: add corrected icd-10 oid

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/common/codesystems/CodeSystemIcd10Se.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/common/codesystems/CodeSystemIcd10Se.java
@@ -8,9 +8,23 @@ public class CodeSystemIcd10Se {
     throw new IllegalStateException("Utility class");
   }
 
-  public static final String ICD_10_SE_CODE_SYSTEM = "1.2.752.116.1.1.1.1.8";
+  private static final String DEPRECATED_ICD_10_SE_CODE_SYSTEM = "1.2.752.116.1.1.1.1.8";
+  public static final String ICD_10_SE_CODE_SYSTEM = "1.2.752.116.1.1.1.1.3";
   public static final String DIAGNOS_ICD_10_ID = "ICD_10_SE";
   private static final String DIAGNOS_ICD_10_LABEL = "ICD-10-SE";
+
+  /**
+   * @deprecated Use {@link #terminology()} for all new certificate types. This method returns a
+   * deprecated ICD-10-SE code system OID that was faulty.
+   */
+  @Deprecated
+  public static ElementDiagnosisTerminology deprecatedTerminology() {
+    return new ElementDiagnosisTerminology(
+        DIAGNOS_ICD_10_ID,
+        DIAGNOS_ICD_10_LABEL,
+        DEPRECATED_ICD_10_SE_CODE_SYSTEM
+    );
+  }
 
   public static ElementDiagnosisTerminology terminology() {
     return new ElementDiagnosisTerminology(
