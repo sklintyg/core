d76c45a2fddd7391012d46bcc83ea6e586a28d04
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2025-10-21 13:54:32 +0200

    K1J-1203: Updated alert box to info instead of observe (#592)

--- certificate-service/app/src/test/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/ag114/elements/MessageArbetsformagaAlertTest.java
+++ certificate-service/app/src/test/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/ag114/elements/MessageArbetsformagaAlertTest.java
@@ -31,7 +31,7 @@ class MessageArbetsformagaAlertTest {
             ElementMessage.builder()
                 .content(
                     "Om patienten bedöms vara arbetsoförmögen i mer än 14 dagar ska Läkarintyg om arbetsförmåga – arbetsgivaren (AG7804) användas. Intyget skapas från Försäkringskassans läkarintyg för sjukpenning (FK7804) och innehåller motsvarande information.")
-                .level(MessageLevel.OBSERVE)
+                .level(MessageLevel.INFO)
                 .build()
         )
         .build();
