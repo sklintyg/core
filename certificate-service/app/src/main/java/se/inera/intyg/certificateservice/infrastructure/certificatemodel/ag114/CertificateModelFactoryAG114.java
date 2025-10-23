3e3f0faeee3e3230ba51127f0b3e649438aa95af
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2025-10-23 07:55:57 +0200

    K1J-1515: Update AG114 code and display name so it matches code system for kv-intygstyp (#594)

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/ag114/CertificateModelFactoryAG114.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/certificatemodel/ag114/CertificateModelFactoryAG114.java
@@ -97,9 +97,9 @@ public class CertificateModelFactoryAG114 implements CertificateModelFactory {
         .id(AG114_V2_0)
         .type(
             new Code(
-                "AG114",
+                "AG1-14",
                 "b64ea353-e8f6-4832-b563-fc7d46f29548",
-                NAME
+                "Läkarintyg om arbetsförmåga – sjuklöneperiod"
             )
         )
         .typeName(new CertificateType(AG1_14))
