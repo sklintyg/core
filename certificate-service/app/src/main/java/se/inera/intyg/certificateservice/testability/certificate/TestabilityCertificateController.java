06f8971bdcfd276e7899dd8a01ef978a453821ac
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2025-10-30 12:23:59 +0100

    K1J-1543: Provide api for webcert to make it possible to create ts8071 v1 from testability and welcome page (#609)

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/testability/certificate/TestabilityCertificateController.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/testability/certificate/TestabilityCertificateController.java
@@ -7,12 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.inera.intyg.certificateservice.application.certificate.dto.CreateCertificateResponse;
 import se.inera.intyg.certificateservice.infrastructure.errorutil.OptimisticLockErrorHandler;
+import se.inera.intyg.certificateservice.testability.certificate.dto.GetCertificateTypeVersionsResponse;
 import se.inera.intyg.certificateservice.testability.certificate.dto.SupportedCertificateTypesResponse;
 import se.inera.intyg.certificateservice.testability.certificate.dto.TestabilityCertificateRequest;
 import se.inera.intyg.certificateservice.testability.certificate.dto.TestabilityResetCertificateRequest;
@@ -41,5 +43,10 @@ public class TestabilityCertificateController {
   @GetMapping("/types")
   List<SupportedCertificateTypesResponse> getSupportedTypes() {
     return testabilityCertificateService.supportedTypes();
+  }
+
+  @GetMapping("/types/{type}")
+  GetCertificateTypeVersionsResponse getCertificateTypeVersions(@PathVariable String type) {
+    return testabilityCertificateService.getCertificateTypeVersions(type);
   }
 }
