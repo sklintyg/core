502a54db5d61771128b8c012d3edb4c91fd9b9d2
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2025-10-31 08:15:02 +0100

    K1J-1517: Fix AMQ IT test issues (#608)

--- certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/common/setup/MessageListener.java
+++ certificate-service/integration-test/src/test/java/se/inera/intyg/certificateservice/integrationtest/common/setup/MessageListener.java
@@ -1,33 +1,41 @@
 package se.inera.intyg.certificateservice.integrationtest.common.setup;
 
+import static org.awaitility.Awaitility.await;
+
 import jakarta.jms.Message;
 import java.time.Duration;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.testcontainers.shaded.org.awaitility.core.ConditionTimeoutException;
 
 @Slf4j
 public class MessageListener {
 
-  private final BlockingQueue<Message> messages = new LinkedBlockingQueue<>();
+  private static final ConcurrentHashMap<String, Message> CERTIFICATE_INDEX = new ConcurrentHashMap<>();
 
   @JmsListener(destination = "${certificate.event.queue.name}")
-  public void onMessage(Message msg) {
-    log.info("onMessage: {}", msg);
-    messages.add(msg);
-  }
-
-  public void clear() {
-    messages.clear();
-  }
-
-  public Message awaitMessage(Duration timeout) {
+  public void onMessage(Message message) {
     try {
-      return messages.poll(timeout.toMillis(), TimeUnit.MILLISECONDS);
-    } catch (InterruptedException ie) {
-      Thread.currentThread().interrupt();
+      final var certificateId = message.getStringProperty("certificateId");
+      if (certificateId != null) {
+        CERTIFICATE_INDEX.put(certificateId, message);
+      }
+    } catch (Exception e) {
+      log.info("onMessage (instance {}) received but couldn't read properties: {}",
+          System.identityHashCode(this), message);
+    }
+  }
+
+  public Message awaitByCertificateId(Duration timeout, String certificateId) {
+    try {
+      await()
+          .atMost(timeout)
+          .pollInterval(Duration.ofMillis(200))
+          .until(() -> CERTIFICATE_INDEX.containsKey(certificateId));
+      return CERTIFICATE_INDEX.get(certificateId);
+    } catch (ConditionTimeoutException e) {
+      log.debug("Timeout waiting for message with certificateId: {}", certificateId);
       return null;
     }
   }
