9f148a2a155d190dc3ba23388a48f54bf5f5d53c
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-11-04 18:53:08 +0100

    K1J-1548: Update configuration paths and refactor resource handling in UnitAccessConfiguration

--- certificate-service/app/src/test/java/se/inera/intyg/certificateservice/infrastructure/configuration/UnitAccessConfigurationTest.java
+++ certificate-service/app/src/test/java/se/inera/intyg/certificateservice/infrastructure/configuration/UnitAccessConfigurationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.util.ReflectionTestUtils;
 import se.inera.intyg.certificateservice.domain.configuration.unitaccess.dto.CertificateAccessConfiguration;
 import se.inera.intyg.certificateservice.domain.configuration.unitaccess.dto.CertificateAccessUnitConfiguration;
@@ -16,11 +17,10 @@ import se.inera.intyg.certificateservice.domain.configuration.unitaccess.dto.Cer
 @ExtendWith(MockitoExtension.class)
 class UnitAccessConfigurationTest {
 
-  private static final String UNIT_ACCESS_WITH_VALUES = Paths.get("src", "test", "resources",
-      "unitaccess",
+  private static final String UNIT_ACCESS_WITH_VALUES = Paths.get("unitaccess",
       "unit-access-with-values.json").toString();
   private static final String INVALID_PATH = "InvalidPath";
-  private static final String UNIT_ACCESS_WITH_NULL_VALUES = Paths.get("src", "test", "resources",
+  private static final String UNIT_ACCESS_WITH_NULL_VALUES = Paths.get(
       "unitaccess",
       "unit-access-with-null-values.json").toString();
 
@@ -34,7 +34,7 @@ class UnitAccessConfigurationTest {
   @Test
   void shallParseFileWithValues() {
     ReflectionTestUtils.setField(unitAccessConfiguration, "unitAccessConfigurationPath",
-        UNIT_ACCESS_WITH_VALUES);
+        new ClassPathResource(UNIT_ACCESS_WITH_VALUES));
 
     final var expectedCertificateAccessConfigurations = List.of(
         CertificateAccessConfiguration.builder()
@@ -78,7 +78,7 @@ class UnitAccessConfigurationTest {
   @Test
   void shallParseFileWithNullValues() {
     ReflectionTestUtils.setField(unitAccessConfiguration, "unitAccessConfigurationPath",
-        UNIT_ACCESS_WITH_NULL_VALUES);
+        new ClassPathResource(UNIT_ACCESS_WITH_NULL_VALUES));
 
     final var expectedCertificateAccessConfigurations = List.of(
         CertificateAccessConfiguration.builder()
