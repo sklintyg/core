97f764e275c2860f0523f9a7dfa7089be1b10db2
mhornfeldt <michaela.hornfeldt@gmail.com>
2025-11-07 09:59:22 +0100

    K1J-1562: Refactor

--- certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/GeneralPdfSpecification.java
+++ certificate-service/domain/src/main/java/se/inera/intyg/certificateservice/domain/certificatemodel/model/GeneralPdfSpecification.java
@@ -3,10 +3,6 @@ package se.inera.intyg.certificateservice.domain.certificatemodel.model;
 import lombok.Builder;
 import lombok.Value;
 
-/**
- * PDF specification for general PDF generation. Uses a general PDF service to generate PDFs
- * dynamically without a template.
- */
 @Value
 @Builder
 public class GeneralPdfSpecification implements PdfSpecification {
