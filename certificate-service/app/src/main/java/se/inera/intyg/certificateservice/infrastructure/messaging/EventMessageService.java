5b6afdc493cc8273dd7219b7e8a88786592a42af
Erik Ågren <erik.agren@soprasteria.com>
2025-11-26 10:04:14 +0100

    K1J-1646: Add session and trace ID properties to JMS messages

--- certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/messaging/EventMessageService.java
+++ certificate-service/app/src/main/java/se/inera/intyg/certificateservice/infrastructure/messaging/EventMessageService.java
@@ -1,10 +1,14 @@
 package se.inera.intyg.certificateservice.infrastructure.messaging;
 
+import static se.inera.intyg.certificateservice.logging.MdcLogConstants.SESSION_ID_KEY;
+import static se.inera.intyg.certificateservice.logging.MdcLogConstants.TRACE_ID_KEY;
+
 import jakarta.jms.JMSException;
 import jakarta.jms.Session;
 import jakarta.jms.TextMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.domain.event.model.CertificateEvent;
@@ -49,6 +53,8 @@ public class EventMessageService implements CertificateEventSubscriber, MessageE
     message.setStringProperty("messageId", messageId);
     message.setStringProperty("certificateId", certificateId);
     message.setStringProperty("eventType", eventType);
+    message.setStringProperty(SESSION_ID_KEY, MDC.get(SESSION_ID_KEY));
+    message.setStringProperty(TRACE_ID_KEY, MDC.get(TRACE_ID_KEY));
     return message;
   }
 
@@ -58,6 +64,8 @@ public class EventMessageService implements CertificateEventSubscriber, MessageE
     final var message = session.createTextMessage();
     message.setStringProperty("certificateId", certificateId);
     message.setStringProperty("eventType", eventType);
+    message.setStringProperty(SESSION_ID_KEY, MDC.get(SESSION_ID_KEY));
+    message.setStringProperty(TRACE_ID_KEY, MDC.get(TRACE_ID_KEY));
     return message;
   }
 }
