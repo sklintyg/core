9537ba33daeed2d9307a5e19d38ab03bff406f43
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-12-19 14:49:36 +0100

    K1J-1711: Add PdfFontResolver for font resolution in PDF fields

---
+++ certificate-service/pdfbox-generator/src/main/java/se/inera/intyg/certificateservice/pdfboxgenerator/pdf/PdfFontResolver.java
@@ -0,0 +1,40 @@
+package se.inera.intyg.certificateservice.pdfboxgenerator.pdf;
+
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDTextField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDVariableText;
+
+@RequiredArgsConstructor
+public class PdfFontResolver {
+
+  private final PDAcroForm acroForm;
+
+  public PDFont resolveFont(PdfField field) {
+    var extractedField = acroForm.getField(field.getId());
+
+    if (extractedField instanceof PDTextField textField) {
+      return new TextFieldAppearance(textField)
+          .getFont(acroForm.getDefaultResources());
+    }
+
+    if (extractedField instanceof PDVariableText variableText) {
+      return new TextFieldAppearance(variableText)
+          .getFont(acroForm.getDefaultResources());
+    }
+
+    return extractFallbackFont();
+  }
+
+  private PDFont extractFallbackFont() {
+    try {
+      var resources = acroForm.getDefaultResources();
+      var fontName = resources.getFontNames().iterator().next();
+      return resources.getFont(fontName);
+    } catch (IOException e) {
+      throw new IllegalStateException("Could not resolve fallback font", e);
+    }
+  }
+}
