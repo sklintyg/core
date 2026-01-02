2b948b1aaa5bcc67b6a927acf4a1cf284d6d3f1c
Daniel Jönsson <daniel.joensson@soprasteria.com>
2026-01-02 16:26:47 +0100

    K1J-1711: Remove Spring @Component annotation from TextFieldAppearanceFactory to decouple from Spring context

--- certificate-service/pdfbox-generator/src/main/java/se/inera/intyg/certificateservice/pdfboxgenerator/pdf/factory/TextFieldAppearanceFactory.java
+++ certificate-service/pdfbox-generator/src/main/java/se/inera/intyg/certificateservice/pdfboxgenerator/pdf/factory/TextFieldAppearanceFactory.java
@@ -3,10 +3,8 @@ package se.inera.intyg.certificateservice.pdfboxgenerator.pdf.factory;
 import java.util.Optional;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.pdfbox.pdmodel.interactive.form.PDVariableText;
-import org.springframework.stereotype.Component;
 import se.inera.intyg.certificateservice.pdfboxgenerator.pdf.TextFieldAppearance;
 
-@Component
 public class TextFieldAppearanceFactory {
 
   public Optional<TextFieldAppearance> create(PDField field) {
